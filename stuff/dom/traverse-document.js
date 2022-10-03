0e633736ac98012c4ad449a0e53eee216f8374ba
manuelbarzi <manuelbarzi@gmail.com>
2022-10-03 13:11:14 +0200

    add js and dom demos

---
+++ stuff/dom/traverse-document.js
@@ -0,0 +1,40 @@
+// https://b00tc4mp.com
+
+nodes = document.childNodes
+
+for (i = 0; i < nodes.length; i++) {
+    console.log(nodes[i].nodeName)
+
+    nodes2 = nodes[i].childNodes
+
+    for (j = 0; j < nodes2.length; j++) {
+        console.log('  ' + nodes2[j].nodeName)
+
+        nodes3 = nodes2[j].childNodes
+
+        for (k = 0; k < nodes3.length; k++) {
+            console.log('    ' + nodes3[k].nodeName)
+        }
+    }
+}
+VM3823:4 html
+VM3823:4 HTML
+VM3823:9   HEAD
+VM3823:14     #text
+VM3823:14     META
+VM3823:14     #text
+VM3823:14     META
+VM3823:14     #text
+VM3823:14     TITLE
+VM3823:14     #text
+VM3823:14     STYLE
+VM3823:14     #text
+VM3823:14     SCRIPT
+VM3823:9   #text
+VM3823:9   BODY
+VM3823:14     #text
+VM3823:14     H1
+VM3823:14     #text
+VM3823:14     P
+VM3823:14     #text
+undefined
---
+++ stuff/js/refs-vs-vals.js
@@ -0,0 +1,105 @@
+x = 10
+10
+y = 20
+20
+z = x + y
+30
+y  = 21
+21
+z
+30
+z = x + y
+31
+o = new Object
+{}
+o = {}
+{}
+o = new Object
+{}
+o.name = 'Peter'
+'Peter'
+x.name = 'Wendy'
+'Wendy'
+x
+10
+x.name
+undefined
+p.name = 'James'
+VM2623:1 Uncaught ReferenceError: p is not defined
+    at <anonymous>:1:1
+(anonymous) @ VM2623:1
+o.surname = 'Pan'
+'Pan'
+p = 0
+0
+p = o
+{name: 'Peter', surname: 'Pan'}
+p.name = 'Wendy'
+'Wendy'
+o
+{name: 'Wendy', surname: 'Pan'}
+p
+{name: 'Wendy', surname: 'Pan'}
+v = x
+10
+q = {} // new Object
+{}
+q.name = 'James'
+'James'
+q.surname = 'Hook'
+'Hook'
+o.enemy = q
+{name: 'James', surname: 'Hook'}
+o
+{name: 'Wendy', surname: 'Pan', enemy: {…}}
+p
+{name: 'Wendy', surname: 'Pan', enemy: {…}}
+q
+{name: 'James', surname: 'Hook'}
+o.enemy.age = 40
+40
+q
+{name: 'James', surname: 'Hook', age: 40}
+q.age = 45
+45
+o.enemy.age
+45
+p.enemy.age
+45
+function hello() {
+    console.log('Hello!')
+}
+undefined
+hello()
+VM4050:2 Hello!
+undefined
+hello.num = 10
+10
+hello
+ƒ hello() {
+    console.log('Hello!')
+}
+hello.num
+10
+q.salute = hello
+ƒ hello() {
+    console.log('Hello!')
+}
+p.enemy.salute()
+VM4050:2 Hello!
+undefined
+q
+{name: 'James', surname: 'Hook', age: 45, salute: ƒ}
+p
+{name: 'Wendy', surname: 'Pan', enemy: {…}}enemy: {name: 'James', surname: 'Hook', age: 45, salute: ƒ}name: "Wendy"surname: "Pan"[[Prototype]]: Object
+o.enemy.salute()
+VM4050:2 Hello!
+undefined
+p.enemy.salute.age = z
+31
+x === 10
+true
+p === q
+false
+o === p
+true
