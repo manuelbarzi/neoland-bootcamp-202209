e89e47d6e7417aa212c941b825e8812c15ef8cf2
Cristina Márquez <cristinamarqizq@gmail.com>
2022-10-04 18:02:31 +0200

    old version of vanillaapp, in two .js

---
+++ staff/cristina-marquez/vanilla-app-1/Login.js
@@ -0,0 +1,78 @@
+
+
+var form = document.createElement("form")
+
+var email = document.createElement("input")
+email.type = "email"
+email.name = "email"
+email.placeholder = "enter your email"
+
+var emailLabel = document.createElement('label')
+emailLabel.htmlFor = 'email'
+emailLabel.classList = ['label']
+emailLabel.innerText = 'Your email'
+
+
+var password = document.createElement("input")
+password.type = "password"
+password.placeholder = "enter your password"
+
+var passwordLabel = document.createElement('label')
+passwordLabel.htmlFor = 'password'
+passwordLabel.classList = ['label']
+passwordLabel.innerText = 'Your password'
+
+
+
+var button = document.createElement("button")
+button.innerText = "Login"
+
+
+// button.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if (e.altKey) {
+//         alert('ALT was pressed')
+//     } else {
+//         alert('Hello')
+//     }
+// })
+
+
+
+//agregar los elementos al form
+form.append(emailLabel, email, passwordLabel, password, button);
+
+
+// crear un div
+var containerFlex = document.createElement("div")
+
+// agregar estilos
+containerFlex.classList.add("container-Flex");
+form.classList.add("form")
+button.classList.add("button")
+
+//agregar formulario al contenedor
+containerFlex.append(form)
+document.body.append(containerFlex)
+
+
+var anchor = document.createElement("a")
+anchor.href = "register.html"
+anchor.innerText = "Register"
+
+// document.body.append(anchor) 
+containerFlex.append(anchor)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
