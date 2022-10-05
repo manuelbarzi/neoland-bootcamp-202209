79beb2f4d447e2b427a355d9efc9f99d32a872c2
fede-alejandro <fede.alejandro87@gmail.com>
2022-10-05 09:19:15 +0200

    agregando cambios

--- staff/federico-alejandro/vanilla-app/login.js
+++ staff/federico-alejandro/vanilla-app/login.js
@@ -1,3 +1,6 @@
+log('FATAL', 'BOOM')
+
+
 var link = document.createElement ('link')
 link.rel="stylesheet"
 link.href="style.css"
@@ -11,42 +14,61 @@ h1.innerHTML='HOLA!'
 document.body.append(h1)
 
 
-var email = document.createElement('input')
-var labelEmail = document.createElement('label')
-labelEmail.htmlFor = 'email'
-labelEmail.className= 'container__item--left'
-labelEmail.innerText = "E-mail"
-email.type = 'email'
-email.name = 'email'
-email.id = 'email'
-email.placeholder = 'input your email'
+var loginEmail = document.createElement('input')
+var loginLabelEmail = document.createElement('label')
+loginLabelEmail.htmlFor = 'email'
+loginLabelEmail.className= 'container__item--left'
+loginLabelEmail.innerText = "E-mail"
+loginEmail.type = 'email'
+loginEmail.name = 'email'
+loginEmail.id = 'email'
+loginEmail.placeholder = 'input your email'
 
-var password = document.createElement ('input')
-var labelPassword = document.createElement('label')
-labelPassword.htmlFor = 'password'
-labelPassword.innerText = "Password"
-labelPassword.className ='container__item--left'
-password.type = 'password'
-password.id = 'password'
-password.name = ''
-password.placeholder = 'input you password'
+var loginPassword = document.createElement ('input')
+var loginLabelPassword = document.createElement('label')
+loginLabelPassword.htmlFor = 'password'
+loginLabelPassword.innerText = "Password"
+loginLabelPassword.className ='container__item--left'
+loginPassword.type = 'password'
+loginPassword.id = 'password'
+loginPassword.name = ''
+loginPassword.placeholder = 'input you password'
+
+var loginButton = document.createElement('button')
+loginButton.innerText = 'login'
 
 
+var anchor = document.createElement('a')
+anchor.href = "register.html"
+anchor.innerText = "Register"
 
-var button = document.createElement('button')
-button.innerText = 'login'
+loginRegisterLink.onclik = function(event) {
+   event.preventDefault()
+   log('DEBUG', 'navigate to regisrer')
+   loginPage.remove()
+   document.body.append(registerPage)
+}
+
+/*var loginPage = document.createElement ('main')
+loginPage.className = 'container'
+loginPage.append(loginForm, loginRegisterLink)*/
+
+
+  
+/*LOGINREGISTERLINK.ONCLICK = FUNTION {
+    event.preventDefault ()
+    log('DEBUG', navigate to register)
+    loginpage.remove()
+    document.body.append(loginPage o Registerpage) pasar de una pagina a otra 
+}*/
 
 
 
 
 
 var form = document.createElement('form')
-form.append(labelEmail, email, labelPassword, password, button)
+form.append(loginLabelEmail, loginEmail, loginLabelPassword, loginPassword, button)
 document.body.append(form)
 form.className='container'
-
-var anchor = document.createElement('a')
-anchor.href = "register.html"
-anchor.innerText = "Register"
 
 document.body.append(anchor)
