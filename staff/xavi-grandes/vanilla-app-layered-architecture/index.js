e9e2bc20f34168eaec85f3fb2b9f4fa748d28838
XaviGrandes <xavigr92@gmail.com>
2022-10-06 13:04:08 +0200

    I asigned new classes for the body

--- staff/xavi-grandes/vanilla-app-layered-architecture/index.js
+++ staff/xavi-grandes/vanilla-app-layered-architecture/index.js
@@ -4,4 +4,6 @@ var users = [
   { name: "Pepito Grillo", email: "pepito@grillo.com", password: "123123" },
 ];
 
-// document.body.append(homePage)
+
+document.body.className = 'body-login body-register body-home'
+document.body.append(loginPage)
