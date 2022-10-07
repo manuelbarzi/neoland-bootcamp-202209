c403a9b963bec3b8f68b34a669700e1329ab762a
manuelbarzi <manuelbarzi@gmail.com>
2022-10-07 13:25:12 +0200

    add layout for tasks and base code in TODO for tasks CRUD logic

---
+++ staff/manuel-barzi/vanilla-app.11/components/tasks-panel.js
@@ -0,0 +1,7 @@
+var tasksPanel = document.createElement('section')
+tasksPanel.className = 'container container--full-width'
+
+var tasksTitle = document.createElement('h2')
+tasksTitle.innerText = 'Tasks'
+
+tasksPanel.append(tasksTitle)
