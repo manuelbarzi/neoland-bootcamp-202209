1ace807ed9305cd4b7a3eaa513d22d60c9c9b0d1
XaviGrandes <xavigr92@gmail.com>
2022-10-07 17:29:09 +0200

    create tasks panel

---
+++ staff/xavi-grandes/vanilla-app-layered-architecture/componentes/tasks-panel.js
@@ -0,0 +1,19 @@
+var tasksPanel = document.createElement('main')
+    tasksPanel.className='tasks-menu'
+
+var tasksTitle = document.createElement('h1')
+    tasksTitle.innerText='Taks'
+
+var tasksContentPanel = document.createElement('div')
+    tasksContentPanel.className='nombrar'
+
+var tasksTodoColumn = document.createElement('section')
+    tasksTodoColumn.className='nombrar'
+
+var tasksDoingColumn = document.createElement('section')
+    tasksDoingColumn.className='nombrar'
+
+var tasksDoneColumn = document.createElement('section')
+    tasksDoneColumn.className='nombrar'
+
+// tasksContentPanel.append(tasksTodoColumn, tasksDoingColumn, tasksDoneColumn)
