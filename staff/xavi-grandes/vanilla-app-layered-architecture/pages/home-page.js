8d97822ba6147a3e59208fab96b5a2871fc5d29c
XaviGrandes <xavigr92@gmail.com>
2022-10-07 16:13:38 +0200

    insert anchor to logo - insert function to connect settings anchor to settings component

--- staff/xavi-grandes/vanilla-app-layered-architecture/pages/home-page.js
+++ staff/xavi-grandes/vanilla-app-layered-architecture/pages/home-page.js
@@ -1,66 +1,92 @@
-
-var homeHeader = document.createElement ('header')
-  homeHeader.className = 'home-header'
+var homeHeader = document.createElement("header");
+homeHeader.className = "home-header";
 
 // var homeHeaderLink = document.createElement('a')
 //   homeHeaderLink.href = ''
 
-var homeHeaderImage = document.createElement('img')
-homeHeaderImage.className = 'homeImgLogo'
-homeHeaderImage.src = 'https://cdn.iconscout.com/icon/free/png-256/trello-14-1175081.png'
+var homeHeaderLinkImage = document.createElement("a");
+homeHeaderLinkImage.href = " ";
 
-var homeUserNameText = document.createElement ('span')
-  homeUserNameText.innerText = 'User Name'
-  homeUserNameText.className = '#'
+var homeHeaderImage = document.createElement("img");
+homeHeaderImage.className = "homeImgLogo";
+homeHeaderImage.src =
+  "https://cdn.iconscout.com/icon/free/png-256/trello-14-1175081.png";
 
-var homeMenuButton = document.createElement('button')
-  homeMenuButton.className = 'home-header-button material-symbols-outlined'
-  homeMenuButton.innerText = 'menu'
-
-  var homeMenuPanelStatus = 'closed'
-
-  homeMenuButton.onclick = function(){
-    if(homeMenuPanelStatus === 'closed'){
-      
-      document.body.append(homeMenuPanel)
-      
-      homeMenuPanelStatus = 'opened'
-    } else {
-      homeMenuPanel.remove()
-
-      homeMenuPanelStatus = 'closed'
-  }
-  }
-
-
+homeHeaderLinkImage.onclick = function (event) {
+  event.preventDefault();
   
-  var homeMenuPanel = document.createElement ('section')
-  homeMenuPanel.className = 'menu-panel'
+  if (homeMenuPanelStatus === "opened") {
+    homeMenuPanel.remove();
 
-  homeHeader.append(homeHeaderImage, homeUserNameText, homeMenuButton, homeMenuPanel)
-  
-var homeMenuSettingsLink = document.createElement('a')
-  homeMenuSettingsLink.className = 'material-symbols-outlined a-settings'
-  homeMenuSettingsLink.innerText = 'settings'
-  homeMenuSettingsLink.href = ''
-
-var homeMenuButtonLogout = document.createElement ('button')
-  homeMenuButtonLogout.className = 'material-symbols-outlined btn-logout'
-  homeMenuButtonLogout.innerText = 'logout'
-
-  homeMenuButtonLogout.onclick = function() {
-    homeMenuPanel.remove()
-    homeMenuPanelStatus = 'closed'
-    // homeMenuPanel.remove();
-    
-    homePage.remove();
-    document.body.append(loginPage);
-    document.body.className = 'body-login'
+    homeMenuPanelStatus = "closed";
   }
+};
 
-homeMenuPanel.append(homeMenuSettingsLink, homeMenuButtonLogout)
+homeHeaderLinkImage.append(homeHeaderImage);
 
-var homePage = document.createElement('main')
+var homeUserNameText = document.createElement("span");
+homeUserNameText.innerText = "User Name";
+homeUserNameText.className = "#";
 
-homePage.append(homeHeader, homeMenuPanel)
+var homeMenuButton = document.createElement("button");
+homeMenuButton.className = "home-header-button material-symbols-outlined";
+homeMenuButton.innerText = "menu";
+
+var homeMenuPanelStatus = "closed";
+
+homeMenuButton.onclick = function () {
+  if (homeMenuPanelStatus === "closed") {
+    document.body.append(homeMenuPanel);
+
+    homeMenuPanelStatus = "opened";
+  } else {
+    homeMenuPanel.remove();
+
+    homeMenuPanelStatus = "closed";
+  }
+};
+
+var homeMenuPanel = document.createElement("section");
+homeMenuPanel.className = "menu-panel";
+
+homeHeader.append(
+  homeHeaderLinkImage,
+  homeUserNameText,
+  homeMenuButton,
+  // homeMenuPanel
+);
+
+var homeMenuSettingsLink = document.createElement("a");
+homeMenuSettingsLink.className = "material-symbols-outlined a-settings";
+homeMenuSettingsLink.innerText = "settings";
+homeMenuSettingsLink.href = "";
+
+homeMenuSettingsLink.onclick = function (event) {
+  event.preventDefault();
+
+  homeMenuPanel.remove();
+  homeMenuPanelStatus = "closed";
+
+  homePage.append(settingsPanel)
+};
+
+var homeMenuButtonLogout = document.createElement("button");
+homeMenuButtonLogout.className = "material-symbols-outlined btn-logout";
+homeMenuButtonLogout.innerText = "logout";
+
+homeMenuButtonLogout.onclick = function () {
+  homeMenuPanel.remove();
+  homeMenuPanelStatus = "closed";
+  // homeMenuPanel.remove();
+
+  homePage.remove();
+  document.body.append(loginPage);
+  document.body.className = "body-login";
+};
+
+homeMenuPanel.append(homeMenuSettingsLink, homeMenuButtonLogout);
+
+var homePage = document.createElement("main");
+
+homePage.append(homeHeader);
 document.body.append(homePage)
