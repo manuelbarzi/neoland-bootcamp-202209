d1b487968933f33e70af09826956cda1e76d9dfb
XaviGrandes <xavigr92@gmail.com>
2022-10-07 08:40:56 +0200

    I remove var users to users.js and leave index.js only for block and unblock pages

--- staff/xavi-grandes/vanilla-app-layered-architecture/index.js
+++ staff/xavi-grandes/vanilla-app-layered-architecture/index.js
@@ -1,9 +1,6 @@
-log("INFO", "start app");
 
-var users = [
-  { name: "Pepito Grillo", email: "pepito@grillo.com", password: "123123" },
-];
+// document.body.className = 'body-login'
+// document.body.append(loginPage)
 
-
-document.body.className = 'body-login body-register body-home'
-document.body.append(loginPage)
+document.body.className = 'body-home'
+document.body.append(homePage)
