018e35aa6dad9e12f6e64404438b9529f7461a9c
ArmanKardon <armando.alonso.saez@gmail.com>
2022-10-07 15:18:39 +0200

    update version

--- staff/armando-alonso/vanilla-app/logic/update-user-email.js
+++ staff/armando-alonso/vanilla-app/logic/update-user-email.js
@@ -1,15 +1,22 @@
 function updateUserEmail(email, newEmail) {
 
-    for (let i = 0; i < users.length; i++) {
-        const user = users[i];
+    for (var i = 0; i < users.length; i++) {
+        var user = users[i]
 
-        if (newEmail === user.email) {
-            return new Error('Email exists')
-            
-        }
-        if (email === newEmail) {
-            return new Error('This Email is the same that you have')
+        if (user.email === newEmail) {
+            return new Error('user with e-mail ' + newEmail + ' already exists')
         }
     }
-    sessionUser.email = newEmail
+
+    for (var i = 0; i < users.length; i++) {
+        var user = users[i]
+
+        if (user.email === email) {
+            user.email = newEmail
+
+            return null
+        }
+    }
+
+    return new Error('user with e-mail ' + email + ' not found')
 }
