47f3b8a48e938e20eb4be3e5d17140618b9f850a
XaviGrandes <xavigr92@gmail.com>
2022-10-10 15:33:53 +0200

    I separated the menu padel from the body to create a component doc

---
+++ staff/xavi-grandes/vanilla-app-layered-architecture/componentes/menu-panel.js
@@ -0,0 +1,32 @@
+var homeMenuPanel = document.createElement("section");
+  homeMenuPanel.className = "menu-panel";
+  
+var homeMenuSettingsLink = document.createElement("a");
+  homeMenuSettingsLink.className = "material-symbols-outlined a-settings";
+  homeMenuSettingsLink.innerText = "settings";
+  homeMenuSettingsLink.href = "";
+
+homeMenuSettingsLink.onclick = function (event) {
+    event.preventDefault();
+
+    homeMenuPanel.remove();
+    homeMenuPanelStatus = "closed";
+
+    homePage.append(settingsPanel);
+};
+
+var homeMenuButtonLogout = document.createElement("button");
+homeMenuButtonLogout.className = "material-symbols-outlined btn-logout";
+homeMenuButtonLogout.innerText = "logout";
+
+homeMenuButtonLogout.onclick = function () {
+homeMenuPanel.remove();
+homeMenuPanelStatus = "closed";
+  // homeMenuPanel.remove();
+
+homePage.remove();
+document.body.append(loginPage);
+document.body.className = "body-login";
+};
+
+homeMenuPanel.append(homeMenuSettingsLink, homeMenuButtonLogout);
