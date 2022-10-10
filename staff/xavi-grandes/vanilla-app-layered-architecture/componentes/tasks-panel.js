0f1d67f66f0bac5725029f00e7d3e0f5eab068e6
XaviGrandes <xavigr92@gmail.com>
2022-10-10 11:01:50 +0200

    create innerText:h1, classname:div, and innerText to sections TODO, DOING, DONE

--- staff/xavi-grandes/vanilla-app-layered-architecture/componentes/tasks-panel.js
+++ staff/xavi-grandes/vanilla-app-layered-architecture/componentes/tasks-panel.js
@@ -2,18 +2,24 @@ var tasksPanel = document.createElement('main')
     tasksPanel.className='tasks-menu'
 
 var tasksTitle = document.createElement('h1')
-    tasksTitle.innerText='Taks'
+    tasksTitle.className='tasks-title'
+    tasksTitle.innerText='Manage your tasks'
 
 var tasksContentPanel = document.createElement('div')
-    tasksContentPanel.className='nombrar'
+    tasksContentPanel.className='tasks-panel'
 
 var tasksTodoColumn = document.createElement('section')
     tasksTodoColumn.className='nombrar'
+    tasksTodoColumn.innerText='TO DO'
 
 var tasksDoingColumn = document.createElement('section')
     tasksDoingColumn.className='nombrar'
+    tasksDoingColumn.innerText='DOING'
 
 var tasksDoneColumn = document.createElement('section')
     tasksDoneColumn.className='nombrar'
+    tasksDoneColumn.innerText='DONE'
 
-// tasksContentPanel.append(tasksTodoColumn, tasksDoingColumn, tasksDoneColumn)
+tasksContentPanel.append(tasksTodoColumn, tasksDoingColumn, tasksDoneColumn)
+    
+tasksPanel.append(tasksTitle, tasksContentPanel)
