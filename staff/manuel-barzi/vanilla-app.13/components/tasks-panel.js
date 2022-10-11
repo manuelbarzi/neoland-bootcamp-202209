1a97ca9b85de0de24ff240637133b6102220d88d
manuelbarzi <manuelbarzi@gmail.com>
2022-10-11 11:07:23 +0200

    add update task implementation and test

--- staff/manuel-barzi/vanilla-app.13/components/tasks-panel.js
+++ staff/manuel-barzi/vanilla-app.13/components/tasks-panel.js
@@ -11,33 +11,44 @@ var tasksTodoColumn = document.createElement('section')
 tasksTodoColumn.innerText = 'TODO'
 tasksTodoColumn.className = 'container container--border container--padding-s container--items-start'
 
+var taskCard = createTaskCard('buy milk')
+tasksTodoColumn.append(taskCard)
+
+var taskCard2 = createTaskCard('buy eggs')
+tasksTodoColumn.append(taskCard2)
+
+var taskCard3 = createTaskCard('buy tomatoes and bananas')
+tasksTodoColumn.append(taskCard3)
+
 var tasksDoingColumn = document.createElement('section')
 tasksDoingColumn.innerText = 'DOING'
 tasksDoingColumn.className = 'container container--border container--padding-s container--items-start'
+
+var taskCard4 = createTaskCard('buy shoes')
+tasksDoingColumn.append(taskCard4)
+
+var taskCard5 = createTaskCard('buy socks')
+tasksDoingColumn.append(taskCard5)
 
 var tasksDoneColumn = document.createElement('section')
 tasksDoneColumn.innerText = 'DONE'
 tasksDoneColumn.className = 'container container--border container--padding-s container--items-start'
 
+var taskCard6 = createTaskCard('buy gums')
+tasksDoneColumn.append(taskCard6)
+
+var taskCard7 = createTaskCard('buy t-shirt')
+tasksDoneColumn.append(taskCard7)
+
 tasksContentPanel.append(tasksTodoColumn, tasksDoingColumn, tasksDoneColumn)
 
 tasksPanel.append(tasksTitle, tasksContentPanel)
 
-function createTaskCard(taskId, text) {
+function createTaskCard(text) {
     var taskCard = document.createElement('article')
     taskCard.innerText = text
     taskCard.className = 'container container--border container--padding-s container--full-width'
     taskCard.contentEditable = true
-
-    taskCard.onkeyup = function(event) {
-        var result = updateTaskText(user.email, taskId, taskCard.innerText)
-
-        if (result instanceof Error) {
-            alert(result.message)
-
-            return
-        }
-    }
 
     return taskCard
 }
@@ -58,7 +69,7 @@ function renderTasksCards() {
     for (var i = 0; i < myTasks.length; i++) {
         var myTask = myTasks[i]
 
-        var myTaskCard = createTaskCard(myTask.id, myTask.text)
+        var myTaskCard = createTaskCard(myTask.text)
 
         if (myTask.status === 'todo')
             tasksTodoColumn.append(myTaskCard)
--- staff/manuel-barzi/vanilla-app/index.js
+++ staff/manuel-barzi/vanilla-app/index.js
@@ -1,8 +1,12 @@
 log('INFO', 'start app')
 
 var user = null
-// var user = users[2]
-// headerUserNameText.innerText = user.name
 
 document.body.append(loginPage)
-// document.body.append(homePage)
+
+//loginEmailInput.value = 'mickey@mouse.com'
+loginEmailInput.value = 'pepito@grillo.com'
+loginPasswordInput.value = '123123123'
+// loginForm.submit() // WARN this submits the form (preventDefault does not act)
+loginSubmitButton.click()
+
