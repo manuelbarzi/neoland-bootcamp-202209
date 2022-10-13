37fca2969f807d243f9063b43a2a63cb80f54d02
manuelbarzi <manuelbarzi@gmail.com>
2022-10-13 13:10:21 +0200

    addd arrays and strings challenges

--- staff/manuel-barzi/vanilla-app/index.js
+++ staff/manuel-barzi/vanilla-app/index.js
@@ -4,9 +4,11 @@ var user = null
 
 document.body.append(loginPage)
 
+/*
 //loginEmailInput.value = 'mickey@mouse.com'
 loginEmailInput.value = 'pepito@grillo.com'
 loginPasswordInput.value = '123123123'
 // loginForm.submit() // WARN this submits the form (preventDefault does not act)
 loginSubmitButton.click()
+*/
 
