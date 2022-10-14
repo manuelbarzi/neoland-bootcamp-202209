28cbf659b26dd59f2021ef529268ece6277b725b
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-10-14 16:02:15 +0200

    add concat array

---
+++ staff/federico-lopez/arrays/concat.1.js
@@ -0,0 +1,29 @@
+function concat() {
+  debugger;
+  var result = [];
+
+  for (var i = 0; i < array1.length; i++) {
+    result[result.length] = array1[i];
+  }
+
+  for (var i = 0; i < array2.length; i++) {
+    result[result.length] = array2[i];
+  }
+
+  for (var i = 0; i < array3.length; i++) {
+    result[result.length] = array3[i];
+  }
+
+  for (var i = 0; i < array4.length; i++) {
+    result[result.length] = array4[i];
+  }
+
+  for (var i = 0; i < array5.length; i++) {
+    result[result.length] = array5[i];
+  }
+
+  for (var i = 0; i < array6.length; i++) {
+    result[result.length] = array6[i];
+  }
+  return result;
+}
