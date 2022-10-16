2187ed3c6590347947fdcbd8b6f7b408436c19d3
jrodriguezroca <jrodriguezroca@gmail.com>
2022-10-17 01:46:04 +0200

    added a non finished array method -pop-. Only one iteration was needed, but is someway bugged

---
+++ staff/jaume-rodriguez/arrays/pop.js
@@ -0,0 +1,18 @@
+function pop(array){
+    result = array[array.length - 1]
+
+    array.length--
+
+    return result
+}
+
+/* function pop(array){
+
+    for (var i = 0; i < array.length; i++){
+        result = array[i]
+        if (i === array.length - 1){
+            array.length--
+        }
+    }
+    return  result
+} */
