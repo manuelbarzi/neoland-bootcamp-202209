53ce357f9496f0c2551ea5193f390c30bbcad40d
jrodriguezroca <jrodriguezroca@gmail.com>
2022-10-18 00:51:31 +0200

    added new distribution on arrays and string methods. Now files.js are inside their own method type. Ex; find/find.js

--- staff/jaume-rodriguez/strings/repeat/repeat.test.js
+++ staff/jaume-rodriguez/strings/repeat/repeat.test.js
@@ -4,17 +4,11 @@ var string = "hola mundo, "
 
 var result = repeat(string, 2)
 
-console.assert(result === "hola mundo, hola mundo, ")
-
-
 // CASE 2 returns a RangeError when the count is negative
 
 var string = "hola mundo, "
 
 var result = repeat(string, -2)
-
-console.assert(result === "Invalid count  value: -2")
-
 
 // CASE 3 returns rounding down "hola mundo, hola mundo, " when count is a decimal 2.5
 
@@ -22,12 +16,8 @@ var string = "hola mundo, "
 
 var result = repeat(string, 2.5)
 
-console.assert(result === "hola mundo, hola mundo, ")
-
 // CASE 4 returns a RangeError when the count is infinity
 
 var string = "hola mundo, "
 
 var result = repeat(string, 1/0)
-
-console.assert(result === "Invalid count  value: -2")
