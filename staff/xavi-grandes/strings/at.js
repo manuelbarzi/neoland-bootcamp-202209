b825f8bbba06a0dca683fcbce7d90f6be47bf5f3
XaviGrandes <xavigr92@gmail.com>
2022-10-17 15:57:20 +0200

    string function at.()

--- staff/xavi-grandes/strings/at.js
+++ staff/xavi-grandes/strings/at.js
@@ -1,2 +1,16 @@
 // sintaxis at(index)
 
+function at(string, index = 0 ) {
+    if (typeof string !== 'string') throw new TypeError (string + ' is not a string')
+
+    if (typeof index !== 'number'){
+        index = string[0]
+        return index
+    }
+
+  if (index < 0) {
+    index = string.length + index;
+    return string[index];
+  }
+  return string[index];
+}
