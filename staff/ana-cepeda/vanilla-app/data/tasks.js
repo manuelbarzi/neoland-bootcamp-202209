e2efe565812882ac3bd770d3732a37db907dc165
Ana Danela Cepeda Duran <Dany@MacBook-Pro-di.home>
2022-10-18 16:46:12 +0200

    add vanilla-app

---
+++ staff/ana-cepeda/vanilla-app/data/tasks.js
@@ -0,0 +1,10 @@
+var tasks = [
+    { id: 'task-1', user: 'dany@gmail.com', text: 'buy milk', status: 'todo' },
+    { id: 'task-2', user: 'miky@mouse.com', text: 'go to yoga', status: 'todo' },
+    { id: 'task-3', user: 'miky@mouse.com', text: 'prepare for exam', status: 'todo' },
+    { id: 'task-4', user: 'dany@gmail.com', text: 'buy eggs', status: 'doing' },
+    { id: 'task-5', user: 'miky@mouse.com', text: 'visit grandma', status: 'done' },
+    { id: 'task-6', user: 'dany@gmail.com', text: 'buy tomatoes', status: 'done' },
+    { id: 'task-7', user: 'luna@gmail.com', text: 'ask money to donald', status: 'todo' },
+    { id: 'task-8', user: 'luna@gmail.com', text: 'buy present to mini', status: 'doing' },
+]
---
+++ staff/ana-cepeda/vanilla-app/data/users.js
@@ -0,0 +1,5 @@
+var users = [
+    { name: 'Dany Cepeda', email: 'dany@gmail.com', password: '12345678' },
+    { name: 'Luna Rodriguez', email: 'luna@hotmail.com', password: 'asdfghjk' },
+    { name: 'Miky Mouse', email: 'miky@mouse.com', password: 'qwertyui' },
+]
