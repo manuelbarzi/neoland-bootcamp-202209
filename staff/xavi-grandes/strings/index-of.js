81f6889094b8299a85e3b301807baf6352e8b03b
XaviGrandes <xavigr92@gmail.com>
2022-10-18 17:27:30 +0200

    add new string index-of with different cases

--- staff/xavi-grandes/strings/index-of.js
+++ staff/xavi-grandes/strings/index-of.js
@@ -1,36 +1,42 @@
-function indexOf(string, searchString, position = 0 ){
-    // let result =  '' 
-    debugger
+function indexOf(string, searchString, position = 0) {
+  // let result =  ''
 
-    for (let i = 0; i < string.length; i++) {
-        let hasToContinue = true
 
-        for (let j = 0; j < searchString.length && hasToContinue; j++) {
-            if(searchString[j] !== string[i + j]) {
-                hasToContinue = false
-            } else {
-                if(j === searchString.length - 1) {
-                    return i
-                }
-            }
+  for (let i = position; i < string.length; i++) {
+    let hasToContinue = true;
+
+    if (searchString === ''){
+        return position}
+
+    for (let j = 0; j < searchString.length && hasToContinue; j++) {
+      
+        if (searchString[j] !== string[i + j]) {
+        hasToContinue = false;
+      } else {
+        if (j === searchString.length - 1) {
+          return i;
         }
+      }
     }
-
-    // for (var i = 0; i < string.length; i++){
-    //     char = string[i]
-        
-    //     if (char === ' ' || char === ','){
-    //         if (result === searchString){
-    //             resultIndex = i - searchString.length     
-    //             return resultIndex
-    //         } else {
-    //             result = ''; 
-    //             char = string[i + 1];
-    //             i = i + 1
-    //         }
-    //     } 
-
-    //     result += char
-    // }
-    return -1
+  }
+  return -1;
 }
+
+// --------------------------------------------------------------------
+
+// for (var i = 0; i < string.length; i++){
+//     char = string[i]
+
+//     if (char === ' ' || char === ','){
+//         if (result === searchString){
+//             resultIndex = i - searchString.length
+//             return resultIndex
+//         } else {
+//             result = '';
+//             char = string[i + 1];
+//             i = i + 1
+//         }
+//     }
+
+//     result += char
+// }
