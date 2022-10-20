5f3be93644a02a52d837cb230171255659a81956
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-10-20 17:50:21 +0200

    let two versions

--- staff/federico-lopez/array-prototype-x/array.prototype-shuffle.2.js
+++ staff/federico-lopez/array-prototype-x/array.prototype-shuffle.2.js
@@ -1,10 +1,15 @@
 Array.prototype.shuffle = function () {
   const result = [];
+
+  // creo un array de índices donde alojo los índices disponibles
   const indexes = [];
 
   for (let i = 0; i < this.length; i++) {
     indexes[indexes.length] = i;
   }
+
+  // recorrer el array de principio a fin
+  // por cada elemento lo coloco en una nueva posición random
 
   for (let i = 0; i < this.length; i++) {
     const element = this[i];
@@ -15,11 +20,13 @@ Array.prototype.shuffle = function () {
 
     result[index] = element;
 
-    for (let j = randomNumber; j < indexes.length - 1; j++) {
+    for (let j = randomNumber; j < indexes.length - 2; j++) {
       indexes[j] = indexes[j + 1];
     }
-
-    indexes.length--;
   }
+
+  // indexes [0, 1, 2, 3, 4]
+  // result [ empty, empty, 1er elemento, empty, empty ]
+
   return result;
 };
