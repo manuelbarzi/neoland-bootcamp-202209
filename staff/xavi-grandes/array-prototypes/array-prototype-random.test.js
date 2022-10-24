4f92b54f7dcc7aa648b3dcdd32cf81483962d277
XaviGrandes <xavigr92@gmail.com>
2022-10-24 11:15:55 +0200

    new folder for array prototypes

---
+++ staff/xavi-grandes/array-prototypes/array-prototype-random.test.js
@@ -0,0 +1,10 @@
+// CASE returns a random number for an array of numbers
+
+const arrayRandom = [10, 20, 30, 40, 50]
+
+var remix = function mix (){
+    
+}
+
+// var v = a.random()
+// console.assert(a.includes(v))
---
+++ staff/xavi-grandes/array-prototypes/array-prototype-shuffle.js
@@ -0,0 +1,2 @@
+
+array.prototype.shuffle = a_
