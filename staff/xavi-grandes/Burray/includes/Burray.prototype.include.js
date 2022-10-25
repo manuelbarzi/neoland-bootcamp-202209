1112521b7d2a6a46c52b17e2a48cd4ce55197b9f
XaviGrandes <xavigr92@gmail.com>
2022-10-25 15:03:58 +0200

    add new folder Includes to add prototype method includes for Burray

---
+++ staff/xavi-grandes/Burray/includes/Burray.prototype.include.js
@@ -0,0 +1,12 @@
+Burray.prototype.includes = function(b, callback){
+    
+    for (var i = 0; i< this.length; i++)
+        element = this.length[i]
+        callback (element)
+
+        if (callback === true){
+            return true
+        }
+    
+        return false
+}
