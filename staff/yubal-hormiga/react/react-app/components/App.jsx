42bf1bbb2f59f959a05b40d81173f25c5f3e0496
YubalHormiga <yubalhh@gmail.com>
2022-10-27 18:05:23 +0200

    react ract-app

--- staff/yubal-hormiga/react/react-app/components/App.jsx
+++ staff/yubal-hormiga/react/react-app/components/App.jsx
@@ -1,20 +1,35 @@
 class App extends React.Component {
     constructor() {
+        log('INFO', 'App -> constructor')
 
         super()
+
+        this.state = { view: 'register' }
     }
 
     componentDidMount() {
+        log('INFO', 'App -> componentDidMount')
     }
 
     componentWillUnmount() {
+        log('INFO', 'App -> componentWillUnmount')
     }
 
+    navigateToRegister = () => this.setState({ view: 'register' })
+
+    navigateToLogin = () => this.setState({ view: 'login' })
+
+    navigateToHome = () => this.setState({ view: 'home' })
+
     render() {
+        log('INFO', 'App -> render')
 
         return <>
-            {/* <LoginPage /> */}
-            <RegisterPage />
+            {this.state.view === 'login' && <LoginPage onRegisterClick={this.navigateToRegister} onLoggedIn={this.navigateToHome} />}
+            
+            {this.state.view === 'register' && <RegisterPage onLoginClick={this.navigateToLogin}  onRegisterUser={this.navigateToLogin}/>}
+
+            {this.state.view === 'home' && <HomePage onLoggedout ={this.navigateToLogin} />}
         </>
     }
 }
--- staff/yubal-hormiga/react/react-app/pages/RegisterPage.jsx
+++ staff/yubal-hormiga/react/react-app/pages/RegisterPage.jsx
@@ -1,16 +1,61 @@
-function RegisterPage(){
-return <main className="h-screen flex flex-col justify-center items-center">
-    <form className="grid gap-4 grid-cols-4 grid-rows-4 ">
-        <label htmlFor="register-name" className="text-center font-bold text-cyan-900">Name</label>
-        <input type="name" id="register-name" className="focus:outline-0" placeholder="Input your name" required=""/>
-        <label htmlFor="register-email" className="text-center font-bold text-cyan-900">E-mail</label>
-        <input type="email" id="register-email" placeholder="Input your e-mail" className="focus:outline-0" required=""/>
-        <label htmlFor="register-password" className="font-bold text-cyan-900 text-center col-star-2 col-end-3">Password</label>
-        <input type="password" id="register-password" className="focus:outline-0" placeholder="Input your password" pattern="[A-Za-z0-9S]{8,}" required="" title="Use min 8 characters for the password and no spaces"/>
-        <button className="row-start-3 row-end-4 col-start-2 col-end-4 bg-cyan-400 hover:bg-cyan-00 py-1 px-4 font-bold text-gray-100 rounded-md ml-3 hover:bg-blue-600">Register</button>
-    </form>
-    <a href="" className="  text-rose-900 font-bold underline bg-color-red block items-center mt-6 text-center">Login</a>
-</main>
+function RegisterPage(props) {
+    log('INFO', 'RegisterPage -> render')
+
+    const handleClick = event => {
+        event.preventDefault()
+
+        const onLoginClick = props.onLoginClick
+
+        onLoginClick()
+    }
+
+       const handleSubmit = event => {
+        event.preventDefault()
+             
+
+        const form = event.target
+
+        const nameInput =form.name
+        const emailInput = form.email
+        const passwordInput = form.password
+
+        const name = nameInput.value
+        const email = emailInput.value
+        const password = passwordInput.value
+
+        try {
+            registerUser(name, email, password)
+    
+            alert('user registered')
+            
+            const onRegisterUser = props.onRegisterUser
+            onRegisterUser()
+
+        } catch (error) {
+            alert(error.message)
+    
+            passwordInput.value = ''
+        }
+    }
+    
+
+
+
+
+
+
+    return <main className="h-screen flex flex-col justify-center items-center">
+        <form className="grid gap-4 grid-cols-4 grid-rows-4 " onSubmit={handleSubmit}>
+            <label htmlFor="register-name" className="text-center font-bold text-cyan-900">Name</label>
+            <input name = "name" type="name" id="register-name" className="focus:outline-0" placeholder="Input your name" required="" />
+            <label htmlFor="register-email" className="text-center font-bold text-cyan-900">E-mail</label>
+            <input name='email' type="email" id="register-email" placeholder="Input your e-mail" className="focus:outline-0" required="" />
+            <label htmlFor="register-password" className="font-bold text-cyan-900 text-center col-star-2 col-end-3">Password</label>
+            <input name= 'password' type="password" id="register-password" className="focus:outline-0" placeholder="Input your password" pattern="[A-Za-z0-9S]{8,}" required="" title="Use min 8 characters for the password and no spaces" />
+            <button className="row-start-3 row-end-4 col-start-2 col-end-4 bg-cyan-400 hover:bg-cyan-00 py-1 px-4 font-bold text-gray-100 rounded-md ml-3 hover:bg-blue-600">Register</button>
+        </form>
+        <a href="" className="  text-rose-900 font-bold underline bg-color-red block items-center mt-6 text-center" onClick={handleClick}>Login</a>
+    </main>
 
 
 
