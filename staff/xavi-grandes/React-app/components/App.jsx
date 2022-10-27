0b7bdb5658da8ac9afa2f2d13f0f8494785a9b50
XaviGrandes <xavigr92@gmail.com>
2022-10-27 19:50:36 +0200

    add Submit form into RegisterPage

--- staff/xavi-grandes/React-app/components/App.jsx
+++ staff/xavi-grandes/React-app/components/App.jsx
@@ -24,7 +24,7 @@ class App extends React.Component {
       <>
         {this.state.view === "login" && <LoginPage onRegisterClick={this.navigateToRegister} onLoggedIn={this.navigateToHomePage} />}
 
-        {this.state.view === "register" && <RegisterPage onLoginClick={this.navigateToLogin} />}
+        {this.state.view === "register" && <RegisterPage onLoginClick={this.navigateToLogin} onRegisterSubmit={this.navigateToLogin} />}
 
         {this.state.view === "home" && <HomePage onLoggedOut={this.navigateToLogin} />}
       </>
--- staff/xavi-grandes/React-app/pages/RegisterPage.jsx
+++ staff/xavi-grandes/React-app/pages/RegisterPage.jsx
@@ -8,12 +8,36 @@ function RegisterPage(props) {
     onLoginClick()
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    const form = event.target
+
+    const nameInput = form.name
+    const emailInput = form.email
+    const passwordInput = form.password
+
+    const name = nameInput.value
+    const email = emailInput.value
+    const password = passwordInput.value
+
+    try {
+      user = registerUser(name, email, password)
+
+      const onRegisterSubmit = props.onRegisterSubmit
+  
+      onRegisterSubmit()
+
+    } catch (error) {
+      alert (error.message)
+    }
+  }
 
   return (
     <div className="w-64 border rounded-md border-gray-900 flex flex-col p-4 bg-white">
       <h1 className="text-xl font-bold">REGISTER</h1>
       <hr className="border-gray-900 my-2" />
-      <form className="flex flex-col text-center">
+      <form onSubmit={handleSubmit} className="flex flex-col text-center">
         <div className="flex flex-col gap-1 my-1">
           <label className="self-start" htmlFor="name">Name</label>
           <input
