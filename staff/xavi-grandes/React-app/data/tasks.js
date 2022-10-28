28007975fd1202a0d48400e3201c34d491b2074e
XaviGrandes <xavigr92@gmail.com>
2022-10-28 15:19:30 +0200

    update idTasks

--- staff/xavi-grandes/React-app/data/tasks.js
+++ staff/xavi-grandes/React-app/data/tasks.js
@@ -2,7 +2,7 @@ var tasks = [
     { id: 'task-1', user: 'xxxxx@xxxx.com', text: '', status: 'todo' },
     { id: 'task-2', user: 'xxxxx@xxxx.com', text: '', status: 'doing' },
     { id: 'task-3', user: 'xxxxx@xxxx.com', text: '', status: 'done' },
-    { id: 'task-2', user: 'pepito@grillo.com', text: 'buy milk', status: 'todo' },
-    { id: 'task-3', user: 'pepito@grillo.com', text: 'buy eggs', status: 'doing' },
-    { id: 'task-4', user: 'pepito@grillo.com', text: 'buy cereals', status: 'done' },
+    { id: 'task-4', user: 'pepito@grillo.com', text: 'buy milk', status: 'todo' },
+    { id: 'task-5', user: 'pepito@grillo.com', text: 'buy eggs', status: 'doing' },
+    { id: 'task-6', user: 'pepito@grillo.com', text: 'buy cereals', status: 'done' },
 ]
