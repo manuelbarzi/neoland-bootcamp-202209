31884a1e6b94f3b01f9757f8745484483cab219f
Cristina Márquez <cristinamarqizq@gmail.com>
2022-10-29 20:57:12 +0200

    Update task status handlers

--- staff/cristina-marquez/react-app/pages/HomePage.jsx
+++ staff/cristina-marquez/react-app/pages/HomePage.jsx
@@ -1,15 +1,19 @@
+
+//TODO: fix taxt status
+
 class HomePage extends React.Component {
     constructor() {
 
         super()
-
         this.state = { tasks: [] }
 
     }
+
     handleToggleMenu = () => this.setState({ toggleButtonText: this.state.toggleButtonText === 'menu' ? 'close' : 'menu' })
 
 
     handleNavigateToSettings = event => {
+        console.log('Navigating to settings')
         event.preventDefault()
 
         this.setState({ view: 'settings', toggleButtonText: 'menu' })
@@ -47,6 +51,19 @@ class HomePage extends React.Component {
         }
     }
 
+    handleUpdateTaskStatus = (taskId, newStatus) => {
+        try {
+
+            updateTaskStatus(taskId, newStatus)
+
+            const tasks = retrieveTasks(user.email)
+
+            this.setState({ tasks })
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
     handleDeleteTask = (taskId) => {
         try {
             deleteTask(taskId)
@@ -58,7 +75,6 @@ class HomePage extends React.Component {
             alert(error.message)
         }
     }
-
 
 
     handleLogout = () => {
@@ -89,29 +105,72 @@ class HomePage extends React.Component {
 
             <section className="flex flex-col items-center">
                 <h2 className="text-4xl m-10">Tasks</h2>
-                <div className="flex-column sm:flex w-11/12 sm:w-3/4 justify-around">
+                <div className="flex-column sm:flex w-11/12 sm:w-11/12 justify-around">
                     <section className="flex flex-col border-2 mr-5 basis-1/3 rounded-md shadow-md">
                         <div className="task-column-header pt-5 pl-5 pb-5 bg-pink-600 rounded-t-md text-white">TODO</div>
 
 
-                        {this.state.tasks.filter(task => task.status === 'TODO').map(task => <div className="task-component mx-3 my-2 flex justify-between">
-                            <article className="task-card" contentEditable="true" onKeyUp={event => this.handleUpdateTaskText(task.id, event.target.innerText)}>{task.text}</article>
-                            <span className="material-symbols-outlined task-component delete-icon" onClick={() => { this.handleDeleteTask(task.id) }}>delete</span>
+                        {this.state.tasks.filter(task => task.status === 'TODO').map(task => <div className="task-component mx-3 my-2 flex justify-between content-center">
+
+                            <div className="flex content-center pt-3">
+                                <article contentEditable="true" onKeyUp={event => this.handleUpdateTaskText(task.id, event.target.innerText)}>{task.text}</article>
+                            </div>
+                            <div className="flex content-center">
+
+                                <select
+                                    className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                                    onChange={event => this.handleUpdateTaskStatus(task.id, event.target.value)}
+                                >
+                                    <option selected={task.status === 'TODO' ? true : false} value="TODO">Todo</option>
+                                    <option selected={task.status === 'IN_PROGRESS' ? true : false} value="IN_PROGRESS">In progress</option>
+                                    <option selected={task.status === 'COMPLETED' ? true : false} value="COMPLETED">Completed</option>
+                                </select>
+                                <span className="material-symbols-outlined task-component delete-icon pt-2" onClick={() => { this.handleDeleteTask(task.id) }}>delete</span>
+                            </div>
+
+
                         </div>)}
 
                     </section>
                     <section className="flex flex-col border-2 mr-5 basis-1/3 rounded-md shadow-md">
                         <div className="task-column-header pt-5 pl-5 pb-5 bg-pink-600 rounded-t-md text-white">IN PROGRESS</div>
                         {this.state.tasks.filter(task => task.status === 'IN_PROGRESS').map(task => <div className="task-component mx-3 my-2 flex justify-between">
-                            <article className="task-card" contentEditable="true" onKeyUp={event => this.handleUpdateTaskText(task.id, event.target.innerText)}>{task.text}</article>
-                            <span className="material-symbols-outlined task-component delete-icon" onClick={() => { this.handleDeleteTask(task.id) }}>delete</span>
+                            <div className="flex content-center pt-3">
+                                <article contentEditable="true" onKeyUp={event => this.handleUpdateTaskText(task.id, event.target.innerText)}>{task.text}</article>
+                            </div>
+                            <div className="flex">
+                                <select
+                                    className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                                    onChange={event => this.handleUpdateTaskStatus(task.id, event.target.value)}
+                                >
+                                    <option selected={task.status === 'TODO' ? true : false} value="TODO">Todo</option>
+                                    <option selected={task.status === 'IN_PROGRESS' ? true : false} value="IN_PROGRESS">In progress</option>
+                                    <option selected={task.status === 'COMPLETED' ? true : false} value="COMPLETED">Completed</option>
+                                </select>
+                                <span className="material-symbols-outlined task-component delete-icon pt-2" onClick={() => { this.handleDeleteTask(task.id) }}>delete</span>
+                            </div>
                         </div>)}
                     </section>
+
+
                     <section className="flex flex-col border-2 mr-5 basis-1/3 rounded-md shadow-md">
                         <div className="task-column-header pt-5 pl-5 pb-5 bg-pink-600 rounded-t-md text-white">COMPLETED</div>
                         {this.state.tasks.filter(task => task.status === 'COMPLETED').map(task => <div className="task-component mx-3 my-2 flex justify-between">
-                            <article className="task-card" contentEditable="true" onKeyUp={event => this.handleUpdateTaskText(task.id, event.target.innerText)}>{task.text}</article>
-                            <span className="material-symbols-outlined task-component delete-icon" onClick={() => { this.handleDeleteTask(task.id) }}>delete</span>
+                            <div className="flex content-center pt-3">
+                                <article contentEditable="true" onKeyUp={event => this.handleUpdateTaskText(task.id, event.target.innerText)}>{task.text}</article>
+                            </div>
+                            <div className="flex">
+                                <select
+                                    className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600"
+                                    onChange={event => this.handleUpdateTaskStatus(task.id, event.target.value)}
+                                >
+                                    <option selected={task.status === 'TODO' ? true : false} value="TODO">Todo</option>
+                                    <option selected={task.status === 'IN_PROGRESS' ? true : false} value="IN_PROGRESS">In progress</option>
+                                    <option selected={task.status === 'COMPLETED' ? true : false} value="COMPLETED">Completed</option>
+                                </select>
+                                <span className="material-symbols-outlined task-component delete-icon pt-2" onClick={() => { this.handleDeleteTask(task.id) }}>delete</span>
+
+                            </div>
                         </div>)}
                     </section>
                 </div>
