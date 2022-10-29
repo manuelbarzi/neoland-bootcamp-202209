f74bfcea7f0404069d30eefb039281e002e10e40
ArmanKardon <armando.alonso.saez@gmail.com>
2022-10-29 14:29:37 +0200

    Update React App

--- staff/armando-alonso/react-app/components/App.jsx
+++ staff/armando-alonso/react-app/components/App.jsx
@@ -27,7 +27,7 @@ class App extends React.Component {
 
         return <>
             {this.state.view === 'login' && <LoginPage onRegisterClick={this.navigateToRegister} onLoggedIn={this.navigateToHome} />}
-            {this.state.view === 'register' && <RegisterPage onLoginClick={this.navigateToLogin} />}
+            {this.state.view === 'register' && <RegisterPage onLoginClick={this.navigateToLogin} onRegister={this.navigateToLogin}/>}
             {this.state.view === 'home' && <HomePage onLoggedOut={this.navigateToLogin} />}
         </>
     }
--- staff/armando-alonso/react-app/pages/RegisterPage.jsx
+++ staff/armando-alonso/react-app/pages/RegisterPage.jsx
@@ -7,19 +7,52 @@ function RegisterPage(props) {
 
         onLoginClick()
     }
+
+    const handleRegister = event => {
+        event.preventDefault()
+
+        const form = event.target
+
+        const nameInput = form.name
+        const emailInput = form.email
+        const passwordInpuut = form.password
+
+        const name = nameInput.value
+        const email = emailInput.value
+        const password = passwordInpuut.value
+
+        try {
+  
+            registerUser(name, email, password)
+          
+            alert("User Registered");
+
+            const onRegister = props.onRegister
+
+            onRegister()
+          
+          } catch (error) {
+          
+            alert(error.message)
+          
+            registerPasswordInput.value = ''
+          }
+    }
+
+
     
 return <main className="h-screen flex flex-col items-center justify-center bg-[#fffde7]">
-    <form className="flex flex-col gap-y-2 rounded-xl shadow-2xl bg-[#cccab5] p-8">
+    <form className="flex flex-col gap-y-2 rounded-xl shadow-2xl bg-[#cccab5] p-8" onSubmit={handleRegister}>
         <h2 className="flex justify-center text-[#ffffff] text-xl mb-3">Register</h2>
         <label htmlFor="user-register" className="container__item--left">User</label>
-        <input type="text" id="user-register" placeholder="User" pattern="[a-zA-Z]{1,}" required="" title="Use characters from A to Z htmlFor names (min 1 character, and not numerics)"/>
+        <input name="name" type="text" id="user-register" placeholder="User" pattern="[a-zA-Z]{1,}" required="" title="Use characters from A to Z htmlFor names (min 1 character, and not numerics)"/>
         <label htmlFor="email-register" className="container__item--left">Email</label>
-        <input type="email" id="email-register" placeholder="Email" required=""/>
+        <input name="email" type="email" id="email-register" placeholder="Email" required=""/>
         <label htmlFor="password-register" className="container__item--left">Password</label>
-        <input type="password" id="password-register" placeholder="Password" pattern="[A-Za-z0-9S]{8,}" required="" title="Use min 8 characters for the password and no spaces"/>
-        <button className="" onClick={handleClick}>Register</button>
+        <input name="password" type="password" id="password-register" placeholder="Password" pattern="[A-Za-z0-9S]{8,}" required="" title="Use min 8 characters for the password and no spaces"/>
+        <button className="">Register</button>
     </form>
-    <a href="">Login</a>
+    <a href="" onClick={handleClick}>Login</a>
 </main>
 
 }
