93fa6e29c555c2d2c337e306f18e53d752d7cfad
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-11-04 14:39:05 +0100

    update

---
+++ staff/federico-lopez/react-app.3/components/Tasks.jsx
@@ -0,0 +1,146 @@
+function Tasks(props) {
+  log("INFO", "Tasks -> render");
+
+  const handleUpdateTaskText = (taskId, newText) => {
+    log("INFO", "Tasks -> handleUpdateTaskText");
+
+    props.onUpdateTaskText(taskId, newText);
+  };
+
+  const handleDeleteTask = (taskId) => {
+    log("INFO", "Tasks -> handleDeleteTask");
+
+    props.onDeleteTask(taskId);
+  };
+
+  const handleUpdateTaskStatus = (taskId, newStatus) => {
+    log("INFO", "Tasks -> handleUpdateTaskStatus");
+
+    props.onUpdateTaskStatus(taskId, newStatus);
+  };
+
+  return (
+    <section className="flex flex-col items-center">
+      <h2>Tasks</h2>
+      <div className="flex flex-col sm:flex-row gap-4">
+        <section className="flex flex-col gap-2 border-2 p-2">
+          <h2>TODO</h2>
+          {props.tasks
+            .filter((task) => task.status === "todo")
+            .map((task) => (
+              <article key={task.id} className="border-2 p-1">
+                <p
+                  suppressContentEditableWarning={true}
+                  contentEditable="true"
+                  onKeyUp={(event) =>
+                    handleUpdateTaskText(task.id, event.target.innerText)
+                  }
+                >
+                  {task.text}
+                </p>
+
+                <button
+                  className="material-symbols-outlined"
+                  onClick={() => handleDeleteTask(task.id)}
+                >
+                  delete
+                </button>
+
+                <select
+                  className="material-symbols-outlined"
+                  onChange={(event) =>
+                    handleUpdateTaskStatus(task.id, event.target.value)
+                  }
+                  defaultValue=""
+                >
+                  <option disabled hidden className="text-sm" value="">
+                    width_normal
+                  </option>
+                  <option value="doing">DOING</option>
+                  <option value="done">DONE</option>
+                </select>
+              </article>
+            ))}
+        </section>
+        <section className="border-2 p-2">
+          <h2>DOING</h2>
+          {props.tasks
+            .filter((task) => task.status === "doing")
+            .map((task) => (
+              <article key={task.id} className="border-2 p-1">
+                <p
+                  suppressContentEditableWarning={true}
+                  contentEditable="true"
+                  onKeyUp={(event) =>
+                    handleUpdateTaskText(task.id, event.target.innerText)
+                  }
+                >
+                  {task.text}
+                </p>
+
+                <button
+                  className="material-symbols-outlined"
+                  onClick={() => handleDeleteTask(task.id)}
+                >
+                  delete
+                </button>
+
+                <select
+                  className="material-symbols-outlined"
+                  onChange={(event) =>
+                    handleUpdateTaskStatus(task.id, event.target.value)
+                  }
+                  defaultValue=""
+                >
+                  <option disabled hidden className="text-sm" value="">
+                    width_normal
+                  </option>
+                  <option value="todo">TODO</option>
+                  <option value="done">DONE</option>
+                </select>
+              </article>
+            ))}
+        </section>
+        <section className="border-2 p-2">
+          <h2>DONE</h2>
+          {props.tasks
+            .filter((task) => task.status === "done")
+            .map((task) => (
+              <article key={task.id} className="border-2 p-1">
+                <p
+                  suppressContentEditableWarning={true}
+                  contentEditable="true"
+                  onKeyUp={(event) =>
+                    handleUpdateTaskText(task.id, event.target.innerText)
+                  }
+                >
+                  {task.text}
+                </p>
+
+                <button
+                  className="material-symbols-outlined"
+                  onClick={() => handleDeleteTask(task.id)}
+                >
+                  delete
+                </button>
+
+                <select
+                  className="material-symbols-outlined"
+                  onChange={(event) =>
+                    handleUpdateTaskStatus(task.id, event.target.value)
+                  }
+                  defaultValue=""
+                >
+                  <option disabled hidden className="text-sm" value="">
+                    width_normal
+                  </option>
+                  <option value="todo">TODO</option>
+                  <option value="doing">DOING</option>
+                </select>
+              </article>
+            ))}
+        </section>
+      </div>
+    </section>
+  );
+}
--- staff/federico-lopez/react-app/components/Header.jsx
+++ staff/federico-lopez/react-app/components/Header.jsx
@@ -1,109 +1,80 @@
 class Header extends React.Component {
-  constructor() {
-    log("INFO", "Header -> constructor");
+    constructor() {
+        log('INFO', 'Header -> constructor')
 
-    super();
+        super()
 
-    this.state = {
-      toggleButtonText: "menu",
-    };
-  }
+        this.state = {
+            toggleButtonText: 'menu'
+        }
+    }
 
-  componentDidMount() {
-    log("INFO", "Header -> componentDidMount");
-  }
+    componentDidMount() {
+        log('INFO', 'Header -> componentDidMount')
+    }
 
-  componentWillUnmount() {
-    log("INFO", "Header -> componentWillUnmount");
-  }
+    componentWillUnmount() {
+        log('INFO', 'Header -> componentWillUnmount')
+    }
 
-  handleNavigateToTasks = (event) => {
-    log("INFO", "Header -> handleNavigateToTasks");
+    handleNavigateToTasks = event => {
+        log('INFO', 'Header -> handleNavigateToTasks')
 
-    event.preventDefault();
+        event.preventDefault()
 
-    this.props.onNavigateToTasks();
-  };
+        this.props.onNavigateToTasks()
+    }
 
-  handleAddTask = () => {
-    log("INFO", "Header -> handleAddTask");
+    handleAddTask = () => {
+        log('INFO', 'Header -> handleAddTask')
+        
+        this.props.onAddTask()
+    }
 
-    this.props.onAddTask();
-  };
+    handleToggleMenu = () => {
+        log('INFO', 'Header -> handleToggleMenu')
 
-  handleToggleMenu = () => {
-    log("INFO", "Header -> handleToggleMenu");
+        this.setState({ toggleButtonText: this.state.toggleButtonText === 'menu' ? 'close' : 'menu' })
+    }
 
-    this.setState({
-      toggleButtonText:
-        this.state.toggleButtonText === "menu" ? "close" : "menu",
-    });
-  };
 
-  handleNavigateToSettings = (event) => {
-    log("INFO", "Header -> handleNavigateToSettings");
+    handleNavigateToSettings = event => {
+        log('INFO', 'Header -> handleNavigateToSettings')
 
-    event.preventDefault();
+        event.preventDefault()
 
-    this.setState({ toggleButtonText: "menu" });
+        this.setState({ toggleButtonText: 'menu' })
 
-    this.props.onNavigateToSettings();
-  };
+        this.props.onNavigateToSettings()
+    }
 
-  handleLogout = () => {
-    log("INFO", "Header -> handleLogout");
+    handleLogout = () => {
+        log('INFO', 'Header -> handleLogout')
 
-    this.props.onLogout();
-  };
+        this.props.onLogout()
+    }
 
-  render() {
-    log("INFO", "Header -> render");
+    render() {
+        log('INFO', 'Header -> render')
 
-    return (
-      <header className="flex flex-col">
-        <div className="flex justify-between">
-          <a href="" onClick={this.handleNavigateToTasks}>
-            <img src="https://fakeimg.pl/50x25/?text=hola%20mundo&amp;font=lobster" />
-          </a>
+        return <header className="flex flex-col">
+            <div className="flex justify-between">
+                <a href="" onClick={this.handleNavigateToTasks}><img src="https://fakeimg.pl/50x25/?text=hola%20mundo&amp;font=lobster" /></a>
 
-          <span>{user && user.name}</span>
+                <span>{user && user.name}</span>
 
-          {this.props.view === "tasks" && (
-            <button
-              className="material-symbols-outlined"
-              onClick={this.handleAddTask}
-            >
-              add
-            </button>
-          )}
+                {this.props.view === 'tasks' && <button className="material-symbols-outlined" onClick={this.handleAddTask}>add</button>}
 
-          <button
-            className="material-symbols-outlined"
-            onClick={this.handleToggleMenu}
-          >
-            {this.state.toggleButtonText}
-          </button>
-        </div>
+                <button className="material-symbols-outlined" onClick={this.handleToggleMenu}>{this.state.toggleButtonText}</button>
+            </div>
 
-        {this.state.toggleButtonText === "close" && (
-          <div className="flex flex-col items-center">
-            <a
-              className="material-symbols-outlined"
-              href=""
-              onClick={this.handleNavigateToSettings}
-            >
-              settings
-            </a>
-
-            <button
-              className="material-symbols-outlined"
-              onClick={this.handleLogout}
-            >
-              logout
-            </button>
-          </div>
-        )}
-      </header>
-    );
-  }
+            {
+                this.state.toggleButtonText === 'close' && <div className="flex flex-col items-center">
+                    <a className="material-symbols-outlined" href="" onClick={this.handleNavigateToSettings}>settings</a>
+                    
+                    <button className="material-symbols-outlined" onClick={this.handleLogout}>logout</button>
+                </div>
+            }
+        </header>
+    }
 }
