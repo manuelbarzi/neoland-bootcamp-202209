50bd01815e10d18ea1cb17c213055a8911cdfca8
i-am-sergio <sergioasensiolamata@hotmail.com>
2022-11-07 15:14:39 +0100

    add vehicle-app

---
+++ staff/sergio-asensio/vehicles-app.1/components/App.jsx
@@ -0,0 +1,43 @@
+const { useState } = React
+
+function App() {
+    const [vehicles, setVehicles] = useState()
+
+    const handleSearch = event => {
+        event.preventDefault()
+
+        const { value: query } = event.target.query
+
+        searchVehicles(query, (error, vehicles) => {
+            if (error) {
+                alert(error.message)
+
+                return
+            }
+
+            setVehicles(vehicles)
+        })
+    }
+
+    return <main className="">
+        <form onSubmit={handleSearch}>
+            <input type="text" name="query" placeholder="criteria" />
+            <button>🔍</button>
+        </form>
+        {
+            !vehicles ?
+                <></>
+                :
+                !vehicles.length ?
+                    <p>no results</p>
+                    :
+                    <ul>
+                        {vehicles.map(vehicle => <li>
+                            <h2>{vehicle.name}</h2>
+                            <img src={vehicle.thumbnail} />
+                            <p>{vehicle.price}</p>
+                        </li>)}
+                    </ul>
+        }
+    </main>
+}
