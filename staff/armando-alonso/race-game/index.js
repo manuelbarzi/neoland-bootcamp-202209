cc71b8f9c9224a5fd8535940d248dd1b5d89a75f
ArmanKardon <armando.alonso.saez@gmail.com>
2022-11-07 19:04:28 +0100

    update race

---
+++ staff/armando-alonso/race-game/index.js
@@ -0,0 +1,53 @@
+const STEP = 5
+
+const rusia = {
+    icon: '🚀',
+    x: 0
+}
+
+const usa = {
+    icon: '🚀',
+    x: 0
+}
+
+
+
+var id = setInterval(() => {
+    console.clear()
+
+    rusia.x += Math.round(Math.random() * STEP)
+    usa.x += Math.round(Math.random() * STEP)
+    if (rusia.x > 100) { 
+        usa.icon = ''
+        clearInterval(id)
+    }
+
+    if (usa.x > 100) { 
+        rusia.icon = ''
+        clearInterval(id)
+    }
+
+    render()
+}, 500)
+
+
+
+function render() {
+
+    if (rusia.x < 100) {
+        console.log(' '.repeat(rusia.x) + rusia.icon)
+    } else {
+        rusia.icon = '💥'
+        console.log(rusia.icon)
+    }
+
+    
+    if (usa.x < 100){
+        console.log(' '.repeat(usa.x) + usa.icon)
+    } else {
+        usa.icon = '💥'
+        console.log(usa.icon)
+    }
+
+    
+}
