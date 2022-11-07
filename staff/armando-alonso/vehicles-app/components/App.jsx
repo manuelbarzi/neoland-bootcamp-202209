cb6043def6fc579ad0b8243285acee83134164b5
ArmanKardon <armando.alonso.saez@gmail.com>
2022-11-07 16:29:37 +0100

    update

--- staff/armando-alonso/vehicles-app/components/App.jsx
+++ staff/armando-alonso/vehicles-app/components/App.jsx
@@ -2,9 +2,15 @@ const { useState } = React
 
 function App () {
     const [view, setView] = useState('search')
+    const [vehicleId, setVehicleId] = useState()
+
+    const handleNavigateToDetail = vehicleId => {
+        setView('detail')
+        setVehicleId(vehicleId)
+    }
 
     return <>
-        {view === 'search' && <Search />}
-        {view === 'detail' && <Detail />}
+        {view === 'search' && <Search onNavigateToDetail={handleNavigateToDetail}/>}
+        {view === 'detail' && <Detail vehicleId={vehicleId}/>}
     </>
 }
