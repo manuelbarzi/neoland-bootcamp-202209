59b27ee79f6106abe8ed52168f5623b1677d9bb5
i-am-sergio <sergioasensiolamata@hotmail.com>
2022-11-08 17:58:30 +0100

    add copy-fily, my-api and race-game

---
+++ staff/sergio-asensio/my-api/index.js
@@ -0,0 +1,29 @@
+const { createServer } = require('http')
+const { readFile } = require('fs')
+
+const PORT = 8080
+
+const api = createServer((req, res) => {
+    readFile('db.json', 'utf8', (error, json) => {
+        if (error) {
+            res.writeHead(500, { 'Content-type': 'application/json' })
+
+            res.end(`{ "error": ${error.message} }`)
+
+            return
+        }
+
+        res.writeHead(200, { 'Content-type': 'application/json' })
+
+        const data = JSON.parse(json)
+
+        // TODO parse query string (param q)
+        // TODO filter data by param q
+        // TODO respond with the results
+
+        res.end(JSON.stringify(data))
+    })
+})
+
+api.listen(PORT)
+console.log('server listen on port 8080')
