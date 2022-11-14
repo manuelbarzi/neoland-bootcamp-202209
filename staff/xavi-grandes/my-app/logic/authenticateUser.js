5a6cb02b8cb43aee10e07fbc028ff3a0bebc309c
XaviGrandes <xavigr92@gmail.com>
2022-11-14 17:00:04 +0100

    typescript the code to verify if the email and password are in the data base of users

---
+++ staff/xavi-grandes/my-app/logic/authenticateUser.js
@@ -0,0 +1,32 @@
+const { readFile } = require('fs')
+
+function authenticateUser(email, password, callback) {
+    if (typeof email !== 'string') throw new TypeError('email is not a string')
+    if (typeof password !== 'string') throw new TypeError ('password is not a string')
+    if (typeof callback !== 'function') throw new TypeError ('callback is not a function')
+
+    readFile('./data/users.json', 'utf-8', (error, json) => {
+        if (error) {
+        callback(error)
+
+        return
+    }
+    
+    const users = JSON.parse(json)
+
+    const user = users.find(user => user.email === email)
+    
+    if (!user){
+        callback(new Error('user not registered'))
+
+        return
+    }
+
+    if (user.password !== password) {
+        callback(new Error('wrong password'))
+        return
+    }
+
+    callback(null, user.id)
+    })
+}
