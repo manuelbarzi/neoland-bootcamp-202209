272ad851133196b6fe0daf0c495470c009fb2d3c
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-11-15 16:55:19 +0100

    update

--- staff/federico-lopez/my-api/index.js
+++ staff/federico-lopez/my-api/index.js
@@ -1,21 +1,21 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
+const express = require("express");
 
-const authPost = require('./handlers/authPost')
-const registerPost = require('./handlers/registerPost')
-const searchGet = require('./handlers/searchGet')
+const authPost = require("./handlers/authPost");
+const registerPost = require("./handlers/registerPost");
+const searchGet = require("./handlers/searchGet");
 
-const jsonBodyParser = require('./utils/jsonBodyParser')
+const jsonBodyParser = require("./utils/jsonBodyParser");
 
-const api = express()
+const api = express();
 
-api.post('/auth', jsonBodyParser, authPost)
+api.post("/auth", jsonBodyParser, authPost);
 
-api.post('/register', jsonBodyParser, registerPost)
+api.post("/register", jsonBodyParser, registerPost);
 
-api.get('/search', searchGet)
+api.get("/search", searchGet);
 
-const { PORT } = process.env
+const { PORT } = process.env;
 
-api.listen(PORT, () => console.log(`server listening on port ${PORT}`))
+api.listen(PORT, () => console.log(`server listening on port ${PORT}`));
--- staff/federico-lopez/my-api/logic/retrieveUser.js
+++ staff/federico-lopez/my-api/logic/retrieveUser.js
@@ -1,28 +1,29 @@
-const { readFile } = require('fs')
+const { readFile } = require("fs");
 
 function retrieveUser(userId, callback) {
-    if (typeof userId !== 'string') throw new TypeError('userId is not a string')
-    if (typeof callback !== 'function') throw new TypeError('callback is not a function')
+  if (typeof userId !== "string") throw new TypeError("userId is not a string");
+  if (typeof callback !== "function")
+    throw new TypeError("callback is not a function");
 
-    readFile('./data/users.json', 'utf8', (error, json) => {
-        if (error) {
-            callback(error)
+  readFile("./data/users.json", "utf8", (error, json) => {
+    if (error) {
+      callback(error);
 
-            return
-        }
+      return;
+    }
 
-        const users = JSON.parse(json)
+    const users = JSON.parse(json);
 
-        const user = users.find(user => user.id === userId)
+    const user = users.find((user) => user.id === userId);
 
-        if (!user) {
-            callback(new Error('user not registered'))
+    if (!user) {
+      callback(new Error("user not registered"));
 
-            return
-        }
+      return;
+    }
 
-        callback(null, user)
-    })
+    callback(null, user);
+  });
 }
 
-module.exports = retrieveUser
+module.exports = retrieveUser;
