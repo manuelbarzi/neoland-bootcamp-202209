35f2fa9a9085fcfcce345530a2a04d0538fe44a4
jrodriguezroca <jrodriguezroca@gmail.com>
2022-11-15 12:44:47 +0100

    build(MyApi): new Api build as full json

--- staff/jaume-rodriguez/MyApi/index.js
+++ staff/jaume-rodriguez/MyApi/index.js
@@ -1,48 +1,21 @@
+require('dotenv').config()
+
 const express = require('express')
-const { readFile } = require('fs')
+
+const authPost = require('./handlers/authPost')
+const registerPost = require('./handlers/registerPost')
+const searchGet = require('./handlers/searchGet')
+
+const jsonBodyParser = require('./utils/jsonBodyParser')
 
 const api = express()
 
-// api.get('/search', (req, res) => res.send('hola mundo')) -> Ejemplo
+api.post('/auth', jsonBodyParser, authPost)
 
+api.post('/register', jsonBodyParser, registerPost)
 
-// http://localhost:8080/MyApi?
-api.get('/MyApi', (req, res) => {
-    readFile('db.json', 'utf8', (error, json) => {
-        if (error) {
-            res.status(500)
-            res.setHeader('Content-type', 'application/json')
-            res.send(`{ "error": ${error.message} }`)
+api.get('/search', searchGet)
 
-            return
-        }
+const { PORT } = process.env
 
-        const data = JSON.parse(json)
-        const { q, imageUrl, code, text } = req.query
-
-        let filtered = data
-
-        // http://localhost:8080/MyApi?q=
-        if (q)
-            filtered = filtered.filter(item => item.imageUrl.includes(q) || item.code.includes(q) || item.text.includes(q))
-
-        // http://localhost:8080/MyApi?imageUrl=
-        if (imageUrl)
-            filtered = filtered.filter(item => item.imageUrl.includes(imageUrl))
-
-        // http://localhost:8080/MyApi?code=
-        if (code)
-            filtered = filtered.filter(item => item.code.includes(code))
-
-        // http://localhost:8080/MyApi?text=
-        if (text)
-            filtered = filtered.filter(item => item.text.includes(text))
-
-        res.status(200)
-        res.setHeader('Content-type', 'application/json')
-        //res.send(JSON.stringify(filtered))
-        res.json(filtered)
-    })
-})
-
-api.listen(8080)
+api.listen(PORT, () => console.log(`server listening on port ${PORT}`))
