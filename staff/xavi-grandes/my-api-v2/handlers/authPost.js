47d89b168c83dc63d841d88f321d8a066d3666a2
XaviGrandes <xavigr92@gmail.com>
2022-11-15 12:39:23 +0100

    Add new folder handlers with authPost.js witch authentificated users

---
+++ staff/xavi-grandes/my-api-v2/handlers/authPost.js
@@ -0,0 +1,23 @@
+const authenticateUser = require('../logic/authenticateUser')
+
+module.exports = (req, res) => {
+    let {email, password } = req.body
+
+    try {
+        authenticateUser (email, password, (error, userId) => {
+            if (error){
+                res.status(500)
+                res.json({ error: error.message })
+                
+                return
+            }
+        
+            // res.setHeader('Content-Type', 'application/json')
+            // res.send(JSON.stringify({ userId }))
+            res.json({ userId })
+        })
+    } catch (error) {
+        res.status(500)
+        res.json({ error: error.message })
+    }
+}
