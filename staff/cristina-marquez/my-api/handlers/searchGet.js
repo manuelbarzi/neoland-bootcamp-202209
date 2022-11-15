facf39c630fea5806a5b1c0c3cbbc741f89f9f99
Cristina Márquez <cristinamarqizq@gmail.com>
2022-11-15 16:56:28 +0100

    my api

---
+++ staff/cristina-marquez/my-api/handlers/searchGet.js
@@ -0,0 +1,21 @@
+const searchHttpCats = require('../logic/searchHttpCats')
+
+module.exports = (req, res) => {
+    try {
+        const { q } = req.query
+
+        searchHttpCats(q, (error, cats) => {
+            if (error) {
+                res.status(500)
+                res.json({ error: error.message })
+
+                return
+            }
+
+            res.json(cats)
+        })
+    } catch (error) {
+        res.status(500)
+        res.json({ error: error.message })
+    }
+}
