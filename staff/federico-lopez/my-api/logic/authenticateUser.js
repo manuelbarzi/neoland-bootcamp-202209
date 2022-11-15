0fe7bf130f03777a4910e5112a8cef3a1890b261
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-11-15 15:15:04 +0100

    update0

--- staff/federico-lopez/my-api/logic/authenticateUser.js
+++ staff/federico-lopez/my-api/logic/authenticateUser.js
@@ -1,37 +1,39 @@
-const { readFile } = require('fs')
+const { readFile } = require("fs");
 
 function authenticateUser(email, password, callback) {
-    if (typeof email !== 'string') throw new TypeError('email is not a string')
-    if (!email.length) throw new Error('email is empty')
-    if (typeof password !== 'string') throw new TypeError('password is not a string')
-    if (!password.length) throw new Error('password is empty')
-    if (typeof callback !== 'function') throw new TypeError('callback is not a function')
+  if (typeof email !== "string") throw new TypeError("email is not a string");
+  if (!email.length) throw new Error("email is empty");
+  if (typeof password !== "string")
+    throw new TypeError("password is not a string");
+  if (!password.length) throw new Error("password is empty");
+  if (typeof callback !== "function")
+    throw new TypeError("callback is not a function");
 
-    readFile('./data/users.json', 'utf8', (error, json) => {
-        if (error) {
-            callback(error)
+  readFile("./data/users.json", "utf8", (error, json) => {
+    if (error) {
+      callback(error);
 
-            return
-        }
+      return;
+    }
 
-        const users = JSON.parse(json)
+    const users = JSON.parse(json);
 
-        const user = users.find(user => user.email === email)
+    const user = users.find((user) => user.email === email);
 
-        if (!user) {
-            callback(new Error('user not registered'))
+    if (!user) {
+      callback(new Error("user not registered"));
 
-            return
-        }
+      return;
+    }
 
-        if (user.password !== password) {
-            callback(new Error('wrong password'))
+    if (user.password !== password) {
+      callback(new Error("wrong password"));
 
-            return
-        }
+      return;
+    }
 
-        callback(null, user.id)
-    })
+    callback(null, user.id);
+  });
 }
 
-module.exports = authenticateUser
+module.exports = authenticateUser;
