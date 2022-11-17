339185cf2e50e8f363d7d778f9226ce53b2fd286
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-11-17 14:36:43 +0100

    update

---
+++ staff/federico-lopez/my-api.6/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const express = require('express')
+
+const authPost = require('./handlers/authPost')
+const registerPost = require('./handlers/registerPost')
+const searchGet = require('./handlers/searchGet')
+
+const jsonBodyParser = require('./utils/jsonBodyParser')
+const cors = require('./utils/cors')
+
+const api = express()
+
+api.use(cors)
+
+api.post('/auth', jsonBodyParser, authPost)
+
+api.post('/register', jsonBodyParser, registerPost)
+
+api.get('/search', searchGet)
+
+const { PORT } = process.env
+
+api.listen(PORT, () => console.log(`server listening on port ${PORT}`))
