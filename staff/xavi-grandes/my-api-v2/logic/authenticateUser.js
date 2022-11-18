e3fd96169ea286072212670b1f82929938b13463
XaviGrandes <xavigr92@gmail.com>
2022-11-18 18:00:52 +0100

    Update: end authenticateUser for api

--- staff/xavi-grandes/my-api-v2/logic/authenticateUser.js
+++ staff/xavi-grandes/my-api-v2/logic/authenticateUser.js
@@ -1,5 +1,13 @@
  const { readFile } = require('fs')
 
+ /**
+ * Authenticates a user against DB
+ * 
+ * @param {string} email The user email
+ * @param {string} password The user password
+ * @param {callback} callback The callback to attend the result
+ */
+
 function authenticateUser (email, password, callback) {
     if (typeof email !== 'string') throw new TypeError ('email is not a string')
     if (!email.length) throw new Error ('email is empty') 
@@ -33,5 +41,14 @@ function authenticateUser (email, password, callback) {
         callback(null, user.id)
     })
 }
+
+/**
+ * Attends the result of the authentication
+ * 
+ * @callback callback
+ * 
+ * @param {Error} error The authentication error
+ * @param {string} userId The id of the user that authenticated
+ */
 
 module.exports = authenticateUser
