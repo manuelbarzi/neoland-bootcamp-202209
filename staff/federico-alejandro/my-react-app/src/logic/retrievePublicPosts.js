949fcbef6ab7613f2ec6bddddae475ced2b03cfe
fede-alejandro <fede.alejandro87@gmail.com>
2022-11-20 21:21:00 +0100

    add button comment

--- staff/federico-alejandro/my-react-app/src/logic/retrievePublicPosts.js
+++ staff/federico-alejandro/my-react-app/src/logic/retrievePublicPosts.js
@@ -1,27 +1,29 @@
-function retrieveUser(userId, callback) {
+function retrievePublicPosts(userId, callback) {
     if (typeof userId !== 'string') throw new TypeError('userId is not a string')
     if (typeof callback !== 'function') throw new TypeError('callback is not a function')
 
-    const xhr = new XMLHttpRequest
+    const xhr = new XMLHttpRequest()
 
     xhr.onload = function () {
-        const { status, responseText: json } = xhr
+        const { status, responeText: json } = xhr
 
         if (status >= 500) {
             const { error } = JSON.parse(json)
-
+            
             callback(new Error(error))
 
             return
         }
 
-        const user = JSON.parse(json)
+        const posts = JSON.parse(json)
 
-        callback(null, user)
+        callback(null, posts)
     }
 
-    xhr.open('GET', 'http://localhost/users')
+    xhr.open('GET', 'http://localhost/posts')
     xhr.setRequestHeader('Authorization', `Bearer ${userId}`)
     xhr.send()
+
 }
-    export default retrieveUser
+
+export default retrievePublicPosts
--- staff/federico-alejandro/my-react-app/src/pages/Home.js
+++ staff/federico-alejandro/my-react-app/src/pages/Home.js
@@ -2,11 +2,16 @@ import log from '../utils/coolog'
 import { useEffect, useState } from 'react'
 import retrieveUser from '../logic/retrieveUser'
 import createPost from '../logic/createPost'
+import retrievePublicPosts from '../logic/retrievePublicPosts'
 
 function Home() {
     log.info('home -> render')
 
     const [user, setUser] = useState()
+    const [posts, setPosts] = useState([])
+    const [toggleButtonPost, setToggleButtonPost] = useState('menu')
+    const [view, setView] = useState()
+
 
     useEffect(() => {
         try {
@@ -46,21 +51,69 @@ function Home() {
         }
     }
 
+    // const close = document.getElementById('close')
+    // const open = document.getElementById('open')
+    // const newPost = document.getElementById('newPost')
+
+    // const openNewPost = () => newPost.classList.add('newPost')
+    // const closeNewPost = () => newPost.classList.remove('newPost')
+
+    const handleTogglePost = () =>
+        setToggleButtonPost(toggleButtonPost === 'newPost' ? 'close' : 'newPost')
+
+
+    useEffect(() => {
+        //const { text: { value: text }, visibility: { value: visibility } } = posts
+        try {
+            retrievePublicPosts(window.userId, (error, posts) => {
+                if (posts.visibility === 'public') {
+
+                    setPosts(posts)
+                }
+
+                if (error) {
+                    alert(error.message)
+
+                    return
+                }
+
+
+            })
+        } catch (error) {
+            alert(error.message)
+        }
+
+    }, [posts])
+
     return <main>
         <h2>Hola {user ? user.name : 'home'}</h2>
 
         <h2>Create Post</h2>
+        <button onClick={handleTogglePost}>Comment</button>
+        {toggleButtonPost === 'close' &&
+            <form onSubmit={handleCreatePost}>
+                <label htmlFor='text'>Text</label>
+                <textarea type='text' name='text' placeholder='input a text'></textarea>
+                <label htmlFor='visibility'>Visibility</label>
+                <select id='visibility' name='visibility'>
+                    <option value='public'>Public</option>
+                    <option value='private'>Private</option>
+                </select>
+                <button>Post</button>
+            </form>}
+        
+            
+        
 
-        <form onSubmit={handleCreatePost}>
-            <label htmlFor='text'>Text</label>
-            <textarea type='text' name='text' placeholder='input a text'></textarea>
-            <label htmlFor='visibility'>Visibility</label>
-            <select id='visibility' name='visibility'>
-                <option value='public'>Public</option>
-                <option value='private'>Private</option>
-            </select>
-            <button>Post</button>
-        </form>
+        <section>
+            <h2>Public Posts</h2>
+            {posts.filter(post => post.visibility === 'public').map(post => <post
+                key={post.id}
+                post={post}
+            />)}
+
+        </section>
+
     </main>
 }
 
