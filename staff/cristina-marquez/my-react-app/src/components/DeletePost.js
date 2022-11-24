4c7edfaf44a1c237d7ab8f7fdd3531c4663afc5d
Cristina Márquez <cristinamarqizq@gmail.com>
2022-11-24 16:04:23 +0100

    delete in front finished

---
+++ staff/cristina-marquez/my-react-app/src/components/DeletePost.js
@@ -0,0 +1,42 @@
+import deletePost from "../logic/deletePost"
+import { AiOutlineCloseCircle } from "react-icons/ai";
+
+
+function DeletePost({ onClose, onUpdated, post }) {
+
+
+    const confirmDeletePost = () => {
+
+
+        try {
+            deletePost(post.id)
+
+            onUpdated()
+        } catch (error) {
+            alert(error.message)
+
+        }
+
+    }
+
+
+
+
+
+
+    return <div className="bg-[#aaaa] absolute top-0 left-0 h-full w-full flex flex-col justify-center items-center overflow-hidden" onClick={onClose}>
+        <div className="bg-[white] p-5 rounded-xl flex flex-col items-end gap-2" onClick={event => event.stopPropagation()}>
+            <AiOutlineCloseCircle size="1.5rem" onClick={onClose} className="cursor-pointer" />
+            <p>Do you want to delete this post?</p>
+            <div className="flex gap-2" >
+
+                <button onClick={confirmDeletePost}>Confirm</button>
+            </div>
+        </div>
+    </div >
+
+}
+
+
+
+export default DeletePost
--- staff/cristina-marquez/my-react-app/src/components/Post.js
+++ staff/cristina-marquez/my-react-app/src/components/Post.js
@@ -1,10 +1,13 @@
 import { useState } from "react"
 import { AiOutlineDelete, AiOutlineEdit } from "react-icons/ai";
 import UpdatePost from "./UpdatePost";
+import DeletePost from "./DeletePost";
 
 function Post({ content, onUpdate }) {
 
     const [updatePostIsVisible, setUpdatePostIsVisible] = useState(false)
+    const [deletePostIsVisible, setDeletePostIsVisible] = useState(false)
+
 
 
     const updatePostHandler = (event) => {
@@ -21,6 +24,15 @@ function Post({ content, onUpdate }) {
         onUpdate()
     }
 
+    const deletePostHandler = (event) => {
+        event.preventDefault()
+        setDeletePostIsVisible(true)
+    }
+
+    const onDeletePostClose = () => {
+        setDeletePostIsVisible(false)
+
+    }
 
 
 
@@ -30,7 +42,8 @@ function Post({ content, onUpdate }) {
                 <h2 className="pt-1 pl-1 pb-1 bg-teal-500 rounded-t-md text-white">{content.user}</h2>
                 <p>{content.text}</p>
                 <p>{new Date(content.date).toLocaleString()}</p>
-                <button> <AiOutlineDelete /></button>
+                <button onClick={deletePostHandler}> <AiOutlineDelete /></button>
+                {deletePostIsVisible && <DeletePost post={content} onUpdated={onUpdatedPost} onClose={onDeletePostClose} ></DeletePost>}
                 <button onClick={updatePostHandler}> <AiOutlineEdit /> </button>
                 {updatePostIsVisible && <UpdatePost post={content} onUpdated={onUpdatedPost} onClose={onUpdatePostClose} ></UpdatePost>}
 
