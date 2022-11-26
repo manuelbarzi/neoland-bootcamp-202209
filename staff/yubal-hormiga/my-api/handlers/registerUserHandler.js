1f041ef6efe8a8dfa64c8dd34c3c59b6eb1eea73
YubalHormiga <yubalhh@gmail.com>
2022-11-26 13:04:13 +0100

    update handlers resgister&authenticate and logic authenticate y register with mongodb

--- staff/yubal-hormiga/my-api/handlers/registerUserHandler.js
+++ staff/yubal-hormiga/my-api/handlers/registerUserHandler.js
@@ -4,15 +4,9 @@ module.exports = (req, res) => {
     const { name, email, password } = req.body
 
     try {
-        registerUser(name, email, password, error => {
-            if (error) {
-                res.status(500).json({ error: error.message })
-
-                return
-            }
-
-            res.status(201).send()
-        })
+        registerUser(name, email, password)
+            .then(() => res.status(201).send())
+            .catch(error => res.status(500).json({ error: error.message }))
     } catch (error) {
         res.status(500).json({ error: error.message })
     }
