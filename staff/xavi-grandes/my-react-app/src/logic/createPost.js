ef8ad8556cafd904b5347522a397a69f95e049b9
XaviGrandes <xavigr92@gmail.com>
2022-11-29 17:23:52 +0100

    Update logic folder

--- staff/xavi-grandes/my-react-app/src/logic/createPost.js
+++ staff/xavi-grandes/my-react-app/src/logic/createPost.js
@@ -1,20 +1,51 @@
 /**
  * Creates a post against API
  * 
- * @param {string} userId the user id
- * @param {string} text the post text
- * @param {string} visibility the visibility of the post
- * @param {function} callback the callback to attend the result
+ * @param {string} userId The user id
+ * @param {string} text The post text
+ * @param {string} visibility The post visibility
+ * @param {callback} callback The callback to attend the result
  */
+ export default function (userId, text, visibility, callback) {
+    if (typeof userId !== 'string') throw new TypeError('userId is not a string')
+    if (!userId.length) throw new Error('userId is empty')
+    if (typeof text !== 'string') throw new TypeError('text is not a string')
+    if (!text.length) throw new Error('text is empty')
+    if (typeof visibility !== 'string') throw new TypeError('visibility is not a string')
+    if (!visibility.length) throw new Error('visibility is empty')
+    if (visibility !== 'public' && visibility !== 'private') throw new Error('invalid visibility')
 
-export default function (userId, text, visibility, callback) {
-    if(typeof userId !== 'string') throw new TypeError('userId is not a string')
-    if(!userId.length) throw new Error('userId is empty')
-    if(typeof text !== 'string') throw new TypeError('text is not a string')
-    if(!text.length) throw new Error ('text is empty')
-    if(typeof visibility !== 'string') throw new TypeError('visibility is not a string')
-    if(!visibility.length) throw new Error('visibility is empty')
-    if(visibility !== 'public' && visibility !== 'private') throw new Error('invalid visibility')
+    if (!callback)
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest
+
+            xhr.onload = () => {
+                const { status, responseText: json } = xhr
+
+                if (status >= 500) {
+                    const { error } = JSON.parse(json)
+
+                    reject(new Error(error))
+
+                    return
+                }
+
+                resolve()
+            }
+
+            xhr.onerror = () => reject(new Error('connection error'))
+
+            xhr.open('POST', 'http://localhost/posts')
+            xhr.setRequestHeader('Authorization', `Bearer ${userId}`)
+            xhr.setRequestHeader('Content-Type', 'application/json')
+
+            const payload = { text, visibility }
+
+            const json = JSON.stringify(payload)
+
+            xhr.send(json)
+        })
+
     if (typeof callback !== 'function') throw new TypeError('callback is not a function')
 
     const xhr = new XMLHttpRequest
@@ -22,24 +53,27 @@ export default function (userId, text, visibility, callback) {
     xhr.onload = () => {
         const { status, responseText: json } = xhr
 
-        if(status >= 500) {
+        if (status >= 500) {
             const { error } = JSON.parse(json)
 
             callback(new Error(error))
 
             return
         }
+
         callback(null)
     }
 
-    xhr.onerror = () => {callback (new Error ('connection error'))}
+    xhr.onerror = () => callback(new Error('connection error'))
 
     xhr.open('POST', 'http://localhost/posts')
-    xhr.setRequestHeader('Autorization', `Bearer ${userId}`)
+    xhr.setRequestHeader('Authorization', `Bearer ${userId}`)
     xhr.setRequestHeader('Content-Type', 'application/json')
 
-    const payload = {text, visibility}
+    const payload = { text, visibility }
+
     const json = JSON.stringify(payload)
+
     xhr.send(json)
 }
 
--- staff/xavi-grandes/my-react-app/src/logic/registerUser.js
+++ staff/xavi-grandes/my-react-app/src/logic/registerUser.js
@@ -8,46 +8,71 @@ import { IS_EMAIL_REGEX, HAS_SPACES_REGEX, IS_ALPHABETICAL_REGEX } from '../util
  * @param {string} password The user password
  * @param {callback} callback The callback to attend the result
  */
-
-function registerUser (name, email, password, callback ) {
-    if (typeof name !== 'string') throw new TypeError ('name is not a string')
+export default function (name, email, password, callback) {
+    if (typeof name !== 'string') throw new Error('name is not a string')
     if (!IS_ALPHABETICAL_REGEX.test(name)) throw new Error('name is not alphabetical')
 
-    if(typeof email !== 'string') throw new TypeError('email is not a string')
-    if(!IS_EMAIL_REGEX.test(email)) throw new Error ('email is not valid')
+    if (typeof email !== 'string') throw new Error('email is not a string')
+    if (!IS_EMAIL_REGEX.test(email)) throw new Error('email is not valid')
 
-    if(typeof password !== 'string') throw new TypeError('password is not a string')
-    if(HAS_SPACES_REGEX.test(password)) throw new Error('password has spaces')
-    if(password.length < 8) throw new Error ('password lenght is less than 8 characters')
+    if (typeof password !== 'string') throw new Error('password is not a string')
+    if (password.length < 8) throw new Error('password length is less than 8')
+    if (HAS_SPACES_REGEX.test(password)) throw new Error('password has spaces')
 
-    if(typeof callback !== 'function') throw new TypeError ('callback is not a function')
+    if (!callback)
+        return new Promise((resolve, reject) => {
+            const xhr = new XMLHttpRequest
+
+            xhr.onload = () => {
+                const { status, responseText: json } = xhr
+
+                if (status >= 500) {
+                    const { error } = JSON.parse(json)
+
+                    reject(new Error(error))
+
+                    return
+                }
+
+                resolve()
+            }
+
+            xhr.onerror = () => reject(new Error('connection error'))
+
+            xhr.open('POST', 'http://localhost/users')
+            xhr.setRequestHeader('Content-Type', 'application/json')
+
+            const payload = { name, email, password }
+
+            const json = JSON.stringify(payload)
+
+            xhr.send(json)
+        })
+
+    if (typeof callback !== 'function') throw new TypeError('callback is not a function')
 
     const xhr = new XMLHttpRequest
 
     xhr.onload = () => {
-        const {status, responseText: json} = xhr
+        const { status, responseText: json } = xhr
 
         if (status >= 500) {
-
-            // con parse convertimos el string de json en objeto
-            // del objeto sacamos la propiedad error y la guardamos en una variable error con valor error
             const { error } = JSON.parse(json)
-            // convertimos el string en objeto y recojemos la propiedad error, 
 
             callback(new Error(error))
-            //aqui estoy pasando un string de la const anterior
 
             return
         }
 
         callback(null)
     }
+
     xhr.onerror = () => callback(new Error('connection error'))
 
     xhr.open('POST', 'http://localhost/users')
     xhr.setRequestHeader('Content-Type', 'application/json')
 
-    const payload = {name, email, password}
+    const payload = { name, email, password }
 
     const json = JSON.stringify(payload)
 
@@ -56,8 +81,8 @@ function registerUser (name, email, password, callback ) {
 
 /**
  * Attends the result of the register
+ * 
  * @callback callback
- * @param {Error} error The authetication error
+ * 
+ * @param {Error} error The authentication error
  */
-
-export default registerUser
