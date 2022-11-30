849556287b60f81689e648570f929c346c7559b9
XaviGrandes <xavigr92@gmail.com>
2022-11-30 17:20:10 +0100

    Added retrievePostsHandler

---
+++ staff/xavi-grandes/my-api-v3/handlers/retrievePostsHandler.js
@@ -0,0 +1,15 @@
+const retrievePosts = require('../logic/retrievePosts')
+
+module.exports = (req, res) => {
+    const { headers: { authorization }, params: { postId } } =req
+
+    const userId = authorization.substring(7)
+
+    try {
+        retrievePosts(userId, postId)
+        .then (posts => res.json(posts))
+        .catch(error => res.status(500).json({ error: error.message}))
+    } catch(error){
+        res.status(500).json({ error: error.message })
+    }
+}
