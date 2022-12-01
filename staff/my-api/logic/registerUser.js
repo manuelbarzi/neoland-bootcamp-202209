79c27b60e3861f62c3fe03011da141413702cdfc
federicolopezeikilis <federico.lopezeikilis@gmail.com>
2022-12-01 17:20:06 +0100

    update

---
+++ staff/my-api/logic/registerUser.js
@@ -0,0 +1,28 @@
+const context = require('./context')
+
+function registerUser(name, email, password) {
+    if (typeof name !== 'string') throw new TypeError('name is not a string')
+    if (!name.length) throw new Error('name is empty')
+    if (typeof email !== 'string') throw new TypeError('email is not a string')
+    if (!email.length) throw new Error('email is empty')
+    if (typeof password !== 'string') throw new TypeError('password is not a string')
+    if (!password.length) throw new Error('password is empty')
+
+    const { db } = context
+
+    const users = db.collection('users')
+
+    return users.findOne({ email })
+        .then(user => {
+            if (user) throw new Error(`user with email ${email} already exists`)
+
+            return users.insertOne({ name, email, password })
+        })
+        .then(result => {
+            const { acknowledged } = result
+
+            if (!acknowledged) throw new Error(`could not create user`)
+         })
+}
+
+module.exports = registerUser
