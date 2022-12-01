19aadd09d37c4e8a375683632adf2609d299f751
XaviGrandes <xavigr92@gmail.com>
2022-12-01 15:49:19 +0100

    Rename folder my-react-app to my-react-promeses

--- staff/xavi-grandes/my-react-app/src/components/Button.js
+++
@@ -1,3 +0,0 @@
-export default function ({ children, className, onClick }) {
-    return <button className={`border border-black dark:border-white rounded-xl ${className} p-1`} onClick={onClick}>{children}</button>
-}
