f0ff6d37c0c71bb0c042a29f8e19f22994ce7e9c
XaviGrandes <xavigr92@gmail.com>
2022-12-04 18:17:54 +0100

    My-project: Added index and App Code to start the react App

--- staff/xavi-grandes/my-project/app/src/index.js
+++ staff/xavi-grandes/my-project/app/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+//sessionStorage.userId = null
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
