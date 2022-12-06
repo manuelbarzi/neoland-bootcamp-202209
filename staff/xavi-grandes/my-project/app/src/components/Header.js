64a52dd309a3d695c3d7fcae8842719526f9383b
XaviGrandes <xavigr92@gmail.com>
2022-12-06 11:03:01 +0100

    Deleted logout button and move to profile page

--- staff/xavi-grandes/my-project/app/src/components/Header.js
+++ staff/xavi-grandes/my-project/app/src/components/Header.js
@@ -1,14 +1,11 @@
 import log from '../utils/coolog'
-import { AiOutlineLogout, AiOutlineMenu } from 'react-icons/ai'
+import { AiOutlineMenu } from 'react-icons/ai'
 import { IoInvertModeOutline } from 'react-icons/io5'
-import Context from './Context'
-import { useContext, useState } from 'react'
+import { useState } from 'react'
 import Menu from './Menu'
 
 export default function Header({ userName} ) {
     log.info('Header -> render')
-
-    const { logout } = useContext(Context)
 
     const [show, setShow] = useState('close')
     const showMenu = event => {
@@ -23,15 +20,11 @@ export default function Header({ userName} ) {
 
     return <header className="fixed bg-[white] w-full h-[3rem] top-0 flex items-center gap-8 bg-gray-100">
         <button className='p-2 ml-1 flex items-center cursor-pointer' onClick={showMenu}><AiOutlineMenu className='h-[20px] w-[20px]'/></button>
-        <h1>Mis listas</h1>
     { show === 'menu' && <Menu onClose={showMenu}/>}
 
 
         {/* TODO: este variable estará dentro del menú. */}
         <p>{userName}</p>
-
-        {/* TODO: este botón estará en la página del perfil. */}
-        <button onClick={logout}><AiOutlineLogout /></button>
 
         {/* TODO: este botón estará en la página de perfil. */}
         <button onClick={switchMode}><IoInvertModeOutline /></button>
