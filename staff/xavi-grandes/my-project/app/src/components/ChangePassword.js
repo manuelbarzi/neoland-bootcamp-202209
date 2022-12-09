53347938d517acf82bf09bf6c38525e8923e0721
XaviGrandes <xavigr92@gmail.com>
2022-12-09 11:46:55 +0100

    UpdateUserPassword done, todo authenticate Password

--- staff/xavi-grandes/my-project/app/src/components/ChangePassword.js
+++ staff/xavi-grandes/my-project/app/src/components/ChangePassword.js
@@ -1,13 +1,43 @@
+import log from '../utils/coolog'
 import { IoCloseCircle } from 'react-icons/io5'
+// import authenticateUser from '../logic/authenticateUser'
+import updateUserPassword from '../logic/updateUserPasword'
+
+
 export default function ({onClose}) {
 
+    const handleUpdateUserPassword = event => {
+        log.info('Settings -> handleUpdateUserPassword')
+        
+        event.preventDefault()
+
+    const {  newPassword: { value: newPassword } } = event.target
+        
+    try {
+        // authenticateUser(sessionStorage.token, password)
+        // .then(updateUserPassword(sessionStorage.token, newPassword))
+        updateUserPassword(sessionStorage.token, newPassword)
+        .then(() => {
+            alert('Pasword has been changed successfully')
+            onClose()
+        })
+        .catch(error => alert(error.message))
+
+    } catch (error){
+        alert(error.message)
+    }
+
+    }
+
     return <main className="absolute top-0 w-[100%] h-[100vh] bg-[rgba(0,0,0,0.2)] flex justify-center items-center" onClick={onClose}>
-        <div className="bg-white h-[25%] w-3/5 p-2 gap-2 border rounded-xl flex flex-col" onClick={event => event.stopPropagation()}>
+        <div className="bg-white h-[33%] w-3/5 p-2 border rounded-xl flex flex-col" onClick={event => event.stopPropagation()}>
             <i className='self-end'><IoCloseCircle size="1.3rem" onClick={onClose}/></i>
-            <form className="flex flex-col items-center gap-2">
-                <label className="self-start ml-3" htmlFor="email">Pasword</label>
-                <input className="border-b-2 border-black" name="email" type="email" id="email" placeholder="Insera tu nuevo email" defaultValue={'user.password'} />
-                <button className="w-4/5 bg-red-400 border rounded-lg">Save</button>  
+            <form className="flex flex-col items-center gap-2" onSubmit={handleUpdateUserPassword}>
+                <label className="self-start ml-3 text-[#1b385b] font-bold" htmlFor="password">Pasword</label>
+                <input className="border-b-2 border-black" name="password" type="password" id="password" placeholder="Inserta tu password" />
+                <label className="self-start ml-3 text-[#1b385b] font-bold" htmlFor="newPassword">New Pasword</label>
+                <input className="border-b-2 border-black" name="newPassword" type="password" id="newPassword" placeholder="Inserta tu nuevo password"/>
+                <button className="mt-2 w-4/5 bg-[#1b385b] text-white border rounded-lg">Save</button>  
             </form>
         </div>
     </main>
