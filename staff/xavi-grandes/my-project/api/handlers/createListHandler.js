55220192aaf9e69d5c6b1e498e09d52f66196648
XaviGrandes <xavigr92@gmail.com>
2022-12-09 17:14:01 +0100

    Created module export, try createList and catch error

--- staff/xavi-grandes/my-project/api/handlers/createListHandler.js
+++ staff/xavi-grandes/my-project/api/handlers/createListHandler.js
@@ -1,1 +1,15 @@
 const createList = require ('../logic/createlist')
+
+module.exports = (req, res) => {
+    try {
+        const { body: { title }, userId } = req
+
+        createList(userId, title)
+            .then(() => res.status(201).send())
+            .catch(error => res.status(500).json({ error: error.message }))
+            // TODO: ampliar errores
+
+    } catch(error){
+        res.status(500).json({ error: error.message })
+    }
+}
