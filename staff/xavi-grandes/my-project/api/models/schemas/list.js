c7b3701d4a7fcce4788a8d706d6e2f59eef88dc6
XaviGrandes <xavigr92@gmail.com>
2022-12-09 17:12:17 +0100

    Change list to user

--- staff/xavi-grandes/my-project/api/models/schemas/list.js
+++ staff/xavi-grandes/my-project/api/models/schemas/list.js
@@ -2,10 +2,6 @@ const { Schema } = require('mongoose')
 const { Types: { ObjectId } } = Schema
 
 module.exports = new Schema({
-    list: {
-        type: ObjectId,
-        required: true
-    },
     title: {
         type: String,
         required: true
