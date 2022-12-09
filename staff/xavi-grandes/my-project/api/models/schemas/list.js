8175d6fde47bfa057d228192d71d98574fb69be6
XaviGrandes <xavigr92@gmail.com>
2022-12-09 17:16:15 +0100

    Update list

--- staff/xavi-grandes/my-project/api/models/schemas/list.js
+++ staff/xavi-grandes/my-project/api/models/schemas/list.js
@@ -2,6 +2,10 @@ const { Schema } = require('mongoose')
 const { Types: { ObjectId } } = Schema
 
 module.exports = new Schema({
+    user: {
+        type: ObjectId,
+        required: true
+    },
     title: {
         type: String,
         required: true
