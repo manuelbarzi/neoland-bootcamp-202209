469a5ff6a934b85b08f0baf4b839c2134823b1f6
XaviGrandes <xavigr92@gmail.com>
2022-12-09 11:44:28 +0100

    reduce spaces for a clean code

--- staff/xavi-grandes/my-project/api/logic/updateUserPasword.js
+++ staff/xavi-grandes/my-project/api/logic/updateUserPasword.js
@@ -1,15 +1,15 @@
 const { User } = require('../models')
 
-module.exports = function (userId, newPasword) {
+module.exports = function (userId, newPassword) {
     if (typeof userId !== 'string') throw new TypeError('userId is not a string')
     if (!userId.length) throw new Error('userId is empty')
-    if (typeof newPasword !== 'string') throw new TypeError('newPasword is not a string')
-    if (!newPasword.length) throw new Error('newPasword is empty')
+    if (typeof newPassword !== 'string') throw new TypeError('newPassword is not a string')
+    if (!newPassword.length) throw new Error('newPassword is empty')
 
     return User.findById(userId)
     .then(user => {
         if(!user) throw new Error(`user with id ${userId} does not exist`)
 
-        return User.updateOne({ _id: userId}, { $set: {password: newPasword} })
+        return User.updateOne({ _id: userId }, { $set: {password: newPassword} })
     })
 }
