94ef8cc85b52c7c6dceeedf2cb7cb1d93283ba11
XaviGrandes <xavigr92@gmail.com>
2022-12-09 13:12:10 +0100

    Added param password, its filters and use it as filter if(user.password === password)

--- staff/xavi-grandes/my-project/api/logic/updateUserPasword.js
+++ staff/xavi-grandes/my-project/api/logic/updateUserPasword.js
@@ -1,15 +1,20 @@
 const { User } = require('../models')
 
-module.exports = function (userId, newPassword) {
+module.exports = function (userId, password, newPassword) {
     if (typeof userId !== 'string') throw new TypeError('userId is not a string')
     if (!userId.length) throw new Error('userId is empty')
+    if (typeof password !== 'string') throw new TypeError('password is not a string')
+    if (!password.length) throw new Error('password is empty')
     if (typeof newPassword !== 'string') throw new TypeError('newPassword is not a string')
     if (!newPassword.length) throw new Error('newPassword is empty')
 
     return User.findById(userId)
     .then(user => {
         if(!user) throw new Error(`user with id ${userId} does not exist`)
-
-        return User.updateOne({ _id: userId }, { $set: {password: newPassword} })
+        if(user.password === password) {
+            
+            return User.updateOne({ _id: userId }, { $set: {password: newPassword} })
+        }
+        throw new Error('Current password wrong')
     })
 }
