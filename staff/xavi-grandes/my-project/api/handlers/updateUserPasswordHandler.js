e971f0a17c4f0fe1420c48e1455d08b669704bab
XaviGrandes <xavigr92@gmail.com>
2022-12-09 13:10:19 +0100

    Add parameter password for the function updateUserPassword

--- staff/xavi-grandes/my-project/api/handlers/updateUserPasswordHandler.js
+++ staff/xavi-grandes/my-project/api/handlers/updateUserPasswordHandler.js
@@ -2,9 +2,9 @@ const updateUserPassword = require ('../logic/updateUserPasword')
 
 module.exports = (req, res) => {
     try{
-        const { body: { newPassword }, userId } = req
+        const { body: { password, newPassword }, userId } = req
 
-        updateUserPassword(userId, newPassword)
+        updateUserPassword(userId, password, newPassword)
             .then(() => res.status(204).send())
             .catch(error => res.status (500).json({ error: error.message }))
             // TODO: ampliar errores
