68ec41fda7257521d92122f67de772de72cc066d
XaviGrandes <xavigr92@gmail.com>
2022-12-09 16:10:55 +0100

    Change z-index button with value 10 to show down Menu

--- staff/xavi-grandes/my-project/app/src/pages/Home.js
+++ staff/xavi-grandes/my-project/app/src/pages/Home.js
@@ -3,13 +3,17 @@ import { useContext, useEffect, useState } from 'react'
 import Context from '../components/Context'
 import retrieveUser from '../logic/retrieveUser'
 import Header from '../components/Header'
+import CreateListComponent from '../components/CreateListComponent'
 import { errors } from 'com'
 const { FormatError, AuthError, LengthError, NotFoundError } = errors
 
-function Home() {
+export default function Home() {
     log.info('Home -> render')
 
     const [user, setUser] = useState()
+    const [isCreateOpen, setCreateOpen
+    ] = useState(false) 
+
     const { showAlert } = useContext(Context)
 
     useEffect(() => {
@@ -32,12 +36,21 @@ function Home() {
         }
     }, [])
 
+
+    const HandleCreateList = event => {
+        event.preventDefault()
+
+        setCreateOpen(!isCreateOpen)
+    }
+
     return <>
+        {isCreateOpen && <CreateListComponent onClose={HandleCreateList} />}
         {user && <Header userName={user.name}/>}
-        <footer className='-z-10 absolute bottom-0 h-[4rem] flex justify-center items-center w-full bg-gray-200'>
-            <button className='bg-blue-400 h-[3rem] w-2/5 text-white text-xl p-2 flex justify-center items-center rounded-lg'>+ Crear lista</button>
+        <main className='mt-[3rem]'>
+            <p> Aquí irán las listas</p>
+        </main>
+        <footer className='z-10 absolute bottom-0 h-[4rem] flex justify-center items-center w-full bg-gray-200'>
+            <button className=' bg-blue-400 h-[3rem] w-2/5 text-white text-xl p-2 flex justify-center items-center rounded-lg' onClick={HandleCreateList}>+ Crear lista</button>
         </footer>
     </>
 }
-
-export default Home
