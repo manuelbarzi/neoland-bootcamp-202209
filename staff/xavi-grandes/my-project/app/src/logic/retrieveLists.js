13f67c2692d2bc1217c9a45ee8758252f1592bb9
XaviGrandes <xavigr92@gmail.com>
2022-12-12 12:40:22 +0100

    retrieve list with callback has to be change with promises

---
+++ staff/xavi-grandes/my-project/app/src/logic/retrieveLists.js
@@ -0,0 +1,27 @@
+export default function (token, callback) {
+    if (typeof token !== 'string') throw new TypeError('token is not a string')
+    if (!token.length) throw new Error('token is empty')
+    if (typeof callback !== 'function') throw new TypeError('callback is not a function')
+
+    const xhr = new XMLHttpRequest
+
+    xhr.onload = function () {
+        const { status, responseText: json } = xhr
+
+        if (status >= 500) {
+            const { error } = JSON.parse(json)
+
+            callback(new Error(error))
+
+            return
+        }
+
+        const posts = JSON.parse(json)
+
+        callback(null, posts)
+    }
+
+    xhr.open('GET', 'http://localhost/users/lists')
+    xhr.setRequestHeader('Authorization', `Bearer ${token}`)
+    xhr.send()
+}
