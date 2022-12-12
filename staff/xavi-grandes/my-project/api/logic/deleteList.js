f560c4468549eb46329edd3cf13ea0ac4334d373
XaviGrandes <xavigr92@gmail.com>
2022-12-12 15:23:47 +0100

    Added logic first find userId, then listId, then delete

--- staff/xavi-grandes/my-project/api/logic/deleteList.js
+++ staff/xavi-grandes/my-project/api/logic/deleteList.js
@@ -1,5 +1,5 @@
 const { ObjectId } = require('mongodb')
-// const context = require('./context')
+const { User, List} = require('../models')
 
 module.exports = function (userId, listId) {
     if (typeof userId !== 'string') throw new TypeError('userId is not a string')
@@ -7,16 +7,12 @@ module.exports = function (userId, listId) {
     if (typeof listId !== 'string') throw new TypeError('listId is not a string')
     if (!listId.length) throw new Error('listId is empty')
 
-    const { db } = context
-    const users = db.collection('users')
-    const lists = db.collection('lists')
-
-    return users.findOne({ _id: ObjectId(userId) })
+    return User.findById(userId)
         .then(user => {
             if (!user)
                 throw new Error(`user with id ${userId} does not exist`)
 
-            return lists.findOne({ _id: ObjectId(listId) })
+            return List.findOne({ _id: ObjectId(listId) })
         })
         .then(list => {
             if (!list)
@@ -26,11 +22,11 @@ module.exports = function (userId, listId) {
                 throw new Error(`list with id ${listId} does not belong to user with id ${userId}`)
 
 
-            return lists.deleteOne({ _id: ObjectId(listId) })
+            return List.deleteOne({ _id: ObjectId(listId) })
         })
         .then(result => {
             const { acknowledged } = result
 
-            if (!acknowledged) throw new Error(`could not delete post with id ${postId}`)
+            if (!acknowledged) throw new Error(`could not delete post with id ${listId}`)
          })
 }
