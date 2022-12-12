42e0f9fbc6cb4482754589fbc5e0c7cf7ca5f626
XaviGrandes <xavigr92@gmail.com>
2022-12-12 14:58:40 +0100

    No chances - just a test and return

--- staff/xavi-grandes/my-project/api/handlers/retrieveListsHandler.js
+++ staff/xavi-grandes/my-project/api/handlers/retrieveListsHandler.js
@@ -6,8 +6,16 @@ module.exports = (req, res) => {
     
         retrieveLists(userId)
             .then(lists => res.json(lists))
-            .catch(error => res.status(500).json({ error: error.message }))
+            .catch(error => {
+                if (error instanceof NotFoundError)
+                    res.status(404).json({ error: error.message })
+                else
+                    res.status(500).json({ error: error.message })
+            })
     } catch (error) {
-        res.status(500).json({ error: error.message })
+        if (error instanceof TypeError || error instanceof FormatError)
+            res.status(400).json({ error: error.message })
+        else
+            res.status(500).json({ error: error.message })
     }
 }
