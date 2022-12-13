8e0d93b35be9ea393ad0b5ce08d4c85c9de6ee92
XaviGrandes <xavigr92@gmail.com>
2022-12-13 10:03:19 +0100

    Added onclick to toggle CreateProductComponent

--- staff/xavi-grandes/my-project/app/src/pages/MyList.js
+++ staff/xavi-grandes/my-project/app/src/pages/MyList.js
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from "react";
 import Context from "../components/Context";
 import retrieveUser from "../logic/retrieveUser";
 import Header from "../components/Header";
+import CreateProductComponent from "../components/CreateProductComponent";
 import { errors } from "com";
 // import retrieveProducts from "../logic/retrieveProducts";
 const { FormatError, AuthError, LengthError, NotFoundError } = errors;
@@ -12,7 +13,7 @@ export default function Home() {
 
   const [user, setUser] = useState();
 //   const [products, setProducts] = useState();
-
+  const [isCreateOpen, setCreateOpen] = useState(false);
 
   const { showAlert } = useContext(Context);
 
@@ -47,12 +48,16 @@ export default function Home() {
     }
   }, []);
 
+  const toggleCreateProductView = () => {
+    setCreateOpen(!isCreateOpen)
+  }
+
 
 //   const handleCreatedList = () => {
 //     retrieveProducts(sessionStorage.token)
 //       .then((products) => {setLists(products);
 
-//         toggleCreateListView();
+//         toggleCreateProductView();
 //     });
 //   };
 
@@ -73,13 +78,15 @@ export default function Home() {
 
   return (
     <>
+        {isCreateOpen && <CreateProductComponent onClose={toggleCreateProductView} />}
+
       {user && <Header userName={user.name} />}
       <main className="mt-[3rem] flex flex-col gap-2 items-center">
         <p>Aquí irán los productos en lista </p>
       </main>
       <footer className="z-10 fixed bottom-0 h-[4rem] flex justify-center items-center w-full bg-gray-200">
         <button
-          className=" bg-blue-400 h-[3rem] w-2/5 text-white text-xl p-2 flex justify-center items-center rounded-lg">+ Añadir</button>
+          className=" bg-blue-400 h-[3rem] w-2/5 text-white text-xl p-2 flex justify-center items-center rounded-lg" onClick={toggleCreateProductView}>+ Añadir</button>
       </footer>
     </>
   );
