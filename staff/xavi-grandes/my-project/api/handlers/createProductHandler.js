185a6666730580e56afed348fa258f95ab2efb52
XaviGrandes <xavigr92@gmail.com>
2022-12-13 10:52:09 +0100

    Created handler, require title and listId and import createProduct logic

---
+++ staff/xavi-grandes/my-project/api/handlers/createProductHandler.js
@@ -0,0 +1,15 @@
+const createProduct = require ('../logic/createProduct')
+
+module.exports = (req, res) => {
+    try {
+        const { body: { title }, listId } = req
+
+        createProduct(listId, title)
+            .then(() => res.status(201).send())
+            .catch(error => res.status(500).json({ error: error.message }))
+            // TODO: ampliar errores
+
+    } catch(error){
+        res.status(500).json({ error: error.message })
+    }
+}
