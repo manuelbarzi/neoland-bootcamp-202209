da6aa952c2bb2abd247831193249dd6637c9c2f8
XaviGrandes <xavigr92@gmail.com>
2022-12-14 17:51:22 +0100

    Created logic to create a product in the dataBase

---
+++ staff/xavi-grandes/my-project/app/src/logic/createProduct.js
@@ -0,0 +1,40 @@
+/**
+ * Create list box on the home
+ * @param {string} listId 
+ * @param {string} title 
+ */
+
+export default function (listId, title) {
+    if(typeof listId !== 'string') throw new TypeError('Token is not a string')
+    if(!listId.length) throw new Error('listId is empty')
+
+    if(typeof title !== 'string') throw new TypeError ('title is not a string')
+    if(!title.length) throw new Error ('title is empty')
+
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest()
+
+        xhr.onload = () => {
+            const {status, responseText: json} = xhr
+
+            if(status >= 500) {
+                const {error} = JSON.parse(json)
+
+                reject(new Error(error))
+
+                return
+            }
+
+            resolve()
+        }
+
+        xhr.onerror = () => reject(new Error ('connection error'))
+
+        xhr.open ('POST', `http://localhost/product/${listId}`)
+
+        const payload = { title: title }
+        const json = JSON.stringify(payload)
+
+        xhr.send(json)
+    })
+}
