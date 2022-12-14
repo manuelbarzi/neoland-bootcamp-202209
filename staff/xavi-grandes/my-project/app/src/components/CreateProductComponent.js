c0188579bc553924343857316932bbd6057d1f4c
XaviGrandes <xavigr92@gmail.com>
2022-12-14 17:53:31 +0100

    Added function createdProduct

--- staff/xavi-grandes/my-project/app/src/components/CreateProductComponent.js
+++ staff/xavi-grandes/my-project/app/src/components/CreateProductComponent.js
@@ -1,31 +1,32 @@
 import log from '../utils/coolog'
-// import createProduct from '../logic/createProduct'
+import createProduct from '../logic/createProduct'
 
-export default function ({onClose, /*onListCreated*/ }) {
+
+export default function ({onClose, listId, onProductCreated }) {
     log.info('CreateList -> render')
 
-//   const submitCreateList = (event) => {
-//     event.preventDefault();
+  const submitCreateProduct = (event) => {
+    event.preventDefault();
 
-//     const { title: {value: title} } = event.target
+    const { title: {value: title} } = event.target
+  
+    try {
+      createProduct(listId, title)
+      .then(() => {
+          alert('You create a new product')
+          //Change with show alert 
+          onProductCreated()
+      })
+      .catch(error => alert(error.message))
 
-//     try {
-//       createProduct(sessionStorage.token, title)
-//       .then(() => {
-//           alert('You create a list')
-//           //Change with show alert 
-//           onListCreated()
-//       })
-//       .catch(error => alert(error.message))
-
-//     } catch (error){
-//       alert(error.message)
-//     }
-//   } 
+    } catch (error){
+      alert(error.message)
+    }
+ } 
 
   return (
     <section className="absolute top-0 z-30 h-[100vh] w-full bg-blue-200">
-      <form className='mt-4 flex flex-col items-center' /*onSubmit={submitCreateList}*/>
+      <form className='mt-4 flex flex-col items-center' onSubmit={submitCreateProduct}>
         <input className='w-4/5 h-[3rem] rounded-xl pl-4 text-xl' type="text" id="title" name="title" placeholder="Insert title"></input>
         <button className='self-end mr-[2.5rem] mt-4 w-[8.5rem] h-[3rem] bg-green-500 rounded-xl text-xl text-white'>Create</button>
       </form>
