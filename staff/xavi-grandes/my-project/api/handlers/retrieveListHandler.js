c790c655c90e91b729d1be3e1400fb2f527473cf
XaviGrandes <xavigr92@gmail.com>
2022-12-14 15:53:19 +0100

    Created retrieveListHandler

---
+++ staff/xavi-grandes/my-project/api/handlers/retrieveListHandler.js
@@ -0,0 +1,21 @@
+const retrieveList = require('../logic/retrieveList')
+
+module.exports = (req, res) => {
+    try {
+        const { body: { title }, userId } = req
+    
+        retrieveList(userId, title)
+            .then(list => res.json(list))
+            .catch(error => {
+                if (error instanceof NotFoundError)
+                    res.status(404).json({ error: error.message })
+                else
+                    res.status(500).json({ error: error.message })
+            })
+    } catch (error) {
+        if (error instanceof TypeError || error instanceof FormatError)
+            res.status(400).json({ error: error.message })
+        else
+            res.status(500).json({ error: error.message })
+    }
+}
