893cbfad1156398bc9001f774ab7162e67dd82a7
XaviGrandes <xavigr92@gmail.com>
2022-12-16 13:36:04 +0100

    Update status

--- staff/xavi-grandes/my-project/app/src/logic/updateStatus.js
+++ staff/xavi-grandes/my-project/app/src/logic/updateStatus.js
@@ -5,8 +5,7 @@ const { FormatError, UnexpectedError, AuthError, NotFoundError } = errors;
 export default function (itemId, itemStatus) {
   if (typeof itemId !== "string") throw new TypeError("itemId is not a string");
   if (!itemId.length) throw new Error("itemId is empty");
-  if (typeof itemStatus !== "string") throw new TypeError("itemStatus is not a string");
-  if (!itemStatus.length) throw new Error("itemStatus is empty");
+  if (typeof itemStatus !== "boolean") throw new TypeError("itemStatus is not a boolean");
 
   return new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
@@ -14,26 +13,16 @@ export default function (itemId, itemStatus) {
     xhr.onload = function () {
       const { status, responseText: json } = xhr;
 
-      if (status === 200) {
-        const lists = JSON.parse(json);
+      if (status >= 500) {
+        const { error } = JSON.parse(json)
 
-        resolve(lists);
-      } else if (status === 400) {
-        const { error } = JSON.parse(json);
+        reject(new Error(error))
 
-        if (error.includes("is not a")) reject(new TypeError(error));
-        else if (error.includes("empty")) reject(new FormatError(error));
-      } else if (status === 401) {
-        const { error } = JSON.parse(json);
+        return
+    }
 
-        reject(new AuthError(error));
-      } else if (status === 404) {
-        const { error } = JSON.parse(json);
-
-        reject(new NotFoundError(error));
-      } else if (status < 500) reject(new UnexpectedError("client error"));
-      else reject(new UnexpectedError("server error"));
-    };
+    resolve()
+}
 
     xhr.open("PATCH", `http://localhost/items/${itemId}/${itemStatus}`);
 
