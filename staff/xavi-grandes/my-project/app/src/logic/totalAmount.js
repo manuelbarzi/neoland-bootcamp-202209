23bda6ae4aff11912ee75863dcb4c13aaaf53c69
XaviGrandes <xavigr92@gmail.com>
2022-12-19 16:40:37 +0100

    Added if (mapAmountItems[0] === undefined) return 0€

--- staff/xavi-grandes/my-project/app/src/logic/totalAmount.js
+++ staff/xavi-grandes/my-project/app/src/logic/totalAmount.js
@@ -1,11 +1,14 @@
 export default function (items) {
     
     const mapAmountItems = items.map( (item) => item.amount )
-
-    document.getElementById("total").innerHTML = mapAmountItems.reduce(getSum, 0) + '€';
-
-    function getSum(total, num) {
-        return total + num
+    if (mapAmountItems[0] === undefined) {
+        return '0,00€'
+    } else {
+        document.getElementById("total").innerHTML = mapAmountItems.reduce(getSum, 0) + '€';
+    
+        function getSum(total, num) {
+            return total + num
+        }
     }
 
   }
