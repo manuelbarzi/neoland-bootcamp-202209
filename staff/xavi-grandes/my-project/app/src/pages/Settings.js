78ebd7c99256bb2ff9be0001ef738e3ad30604bf
XaviGrandes <xavigr92@gmail.com>
2022-12-20 15:42:03 +0100

    Added select language

--- staff/xavi-grandes/my-project/app/src/pages/Settings.js
+++ staff/xavi-grandes/my-project/app/src/pages/Settings.js
@@ -21,6 +21,13 @@ export default function () {
                     <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                 </label>
             </div>
+            <div className='w-4/5 h-10 flex justify-between items-center'>
+                <label className="ml-3 text-gray-900 dark:text-gray-300" htmlFor="language">Cambiar idioma</label>
+                <select className='m-1 w-24 border border-blue-600' id="language">
+                    <option>Español</option>
+                    <option>Inglés</option>
+                </select>
+            </div>
         </main>
     </>
 }
